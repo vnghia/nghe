6f2b6a55d27c94387a20d0b9f3f8a30361f867d3
Vo Van Nghia <git@vnghia.com>
2024-01-06 14:44:35 +0700

    config: add server config

---
+++ src/config.rs
@@ -0,0 +1,36 @@
+mod built_info {
+    include!(concat!(env!("OUT_DIR"), "/built.rs"));
+}
+
+use serde::Deserialize;
+use std::net::IpAddr;
+
+#[derive(Debug, Deserialize)]
+#[allow(unused)]
+pub struct Server {
+    pub host: IpAddr,
+    pub port: u16,
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(unused)]
+pub struct Config {
+    pub server: Server,
+}
+
+impl Config {
+    pub fn new() -> Result<Self, config::ConfigError> {
+        let s = config::Config::builder()
+            // server
+            .set_default("server.host", "127.0.0.1")?
+            .set_default("server.port", 3000)?
+            .add_source(
+                config::Environment::with_prefix(built_info::PKG_NAME)
+                    .prefix_separator("_")
+                    .separator("__")
+                    .try_parsing(true),
+            )
+            .build()?;
+        s.try_deserialize()
+    }
+}
