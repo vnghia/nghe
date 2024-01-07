ee19c6feeac7697e16d4532f317a2abe85958555
Vo Van Nghia <git@vnghia.com>
2024-01-08 00:01:29 +0700

    os(error): allow overwrite default error message

--- src/open_subsonic/common/error.rs
+++ src/open_subsonic/common/error.rs
@@ -6,10 +6,10 @@ use nghe_proc_macros::wrap_subsonic_response;
 
 pub enum OpenSubsonicError {
     Generic { source: anyhow::Error },
-    BadRequest,
-    Unauthorized,
-    Forbidden,
-    NotFound,
+    BadRequest { message: Option<String> },
+    Unauthorized { message: Option<String> },
+    Forbidden { message: Option<String> },
+    NotFound { message: Option<String> },
 }
 
 #[derive(Debug, Default, Serialize)]
@@ -47,19 +47,22 @@ impl IntoResponse for OpenSubsonicError {
     fn into_response(self) -> Response {
         match self {
             OpenSubsonicError::Generic { source } => error_to_json(0, source.to_string()),
-            OpenSubsonicError::BadRequest {} => {
-                error_to_json(10, "required parameter is missing".to_owned())
-            }
-            OpenSubsonicError::Unauthorized {} => {
-                error_to_json(40, "wrong username or password".to_owned())
-            }
-            OpenSubsonicError::Forbidden {} => error_to_json(
-                50,
-                "user is not authorized for the given operation".to_owned(),
+            OpenSubsonicError::BadRequest { message } => error_to_json(
+                10,
+                message.unwrap_or("required parameter is missing".to_owned()),
             ),
-            OpenSubsonicError::NotFound {} => {
-                error_to_json(70, "the requested data was not found".to_owned())
-            }
+            OpenSubsonicError::Unauthorized { message } => error_to_json(
+                40,
+                message.unwrap_or("wrong username or password".to_owned()),
+            ),
+            OpenSubsonicError::Forbidden { message } => error_to_json(
+                50,
+                message.unwrap_or("user is not authorized for the given operation".to_owned()),
+            ),
+            OpenSubsonicError::NotFound { message } => error_to_json(
+                70,
+                message.unwrap_or("the requested data was not found".to_owned()),
+            ),
         }
         .into_response()
     }
