fc5e4ae8006a7c9162e8efb000e67a5809549ab1
Vo Van Nghia <git@vnghia.com>
2024-01-07 20:29:19 +0700

    os(response): use macro for emit fn

--- src/open_subsonic/common/response.rs
+++ src/open_subsonic/common/response.rs
@@ -1,6 +1,39 @@
 use super::*;
 use serde::{Serialize, Serializer};
 
+macro_rules! emit_constant_serialize {
+    ($constant_name:ident, $constant_type:ty, $constant_value:expr) => {
+        paste::paste! {
+          fn [<emit_ $constant_name>]<S: Serializer>(_: &(), s: S) -> Result<S::Ok, S::Error> {
+            s.[<serialize_ $constant_type>]($constant_value)
+          }
+        }
+    };
+}
+
+macro_rules! wrap_success_response_root {
+  ($struct_name:ident, { $($field_name:ident : $field_type:ty),* }) => {
+      paste::paste! {
+          #[derive(Debug, Default, Serialize)]
+          #[serde(rename_all = "camelCase")]
+          struct [<Actual $struct_name>] {
+              $($field_name : $field_type,)*
+              #[serde(flatten)]
+              constant: SuccessConstantResponse,
+          }
+
+          #[derive(Debug, Default, Serialize)]
+          #[serde(rename_all = "camelCase")]
+          pub struct $struct_name {
+              #[serde(rename = "subsonic-response")]
+              subsonic_response: [<Actual $struct_name>],
+          }
+      }
+  };
+}
+
+pub(crate) use wrap_success_response_root;
+
 #[derive(Debug, Default, Serialize)]
 #[serde(rename_all = "camelCase")]
 struct ConstantResponse {
@@ -17,22 +50,6 @@ struct ConstantResponse {
     open_subsonic: (),
 }
 
-fn emit_open_subsonic_version<S: Serializer>(_: &(), s: S) -> Result<S::Ok, S::Error> {
-    s.serialize_str(constant::OPEN_SUBSONIC_VERSION)
-}
-
-fn emit_server_type<S: Serializer>(_: &(), s: S) -> Result<S::Ok, S::Error> {
-    s.serialize_str(constant::SERVER_TYPE)
-}
-
-fn emit_server_version<S: Serializer>(_: &(), s: S) -> Result<S::Ok, S::Error> {
-    s.serialize_str(constant::SERVER_VERSION)
-}
-
-fn emit_open_subsonic_support<S: Serializer>(_: &(), s: S) -> Result<S::Ok, S::Error> {
-    s.serialize_bool(true)
-}
-
 #[derive(Debug, Default, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct SuccessConstantResponse {
@@ -41,10 +58,6 @@ pub struct SuccessConstantResponse {
 
     #[serde(flatten)]
     constant: ConstantResponse,
-}
-
-fn emit_status_ok<S: Serializer>(_: &(), s: S) -> Result<S::Ok, S::Error> {
-    s.serialize_str("ok")
 }
 
 #[derive(Debug, Default, Serialize)]
@@ -57,29 +70,9 @@ pub struct ErrorConstantResponse {
     constant: ConstantResponse,
 }
 
-fn emit_status_failed<S: Serializer>(_: &(), s: S) -> Result<S::Ok, S::Error> {
-    s.serialize_str("failed")
-}
-
-macro_rules! wrap_success_response_root {
-    ($struct_name:ident, { $($field_name:ident : $field_type:ty),* }) => {
-        paste::paste! {
-          #[derive(Debug, Default, Serialize)]
-          #[serde(rename_all = "camelCase")]
-          struct [<Actual $struct_name>] {
-            $($field_name : $field_type,)*
-            #[serde(flatten)]
-            constant: SuccessConstantResponse,
-          }
-
-          #[derive(Debug, Default, Serialize)]
-          #[serde(rename_all = "camelCase")]
-          pub struct $struct_name {
-              #[serde(rename = "subsonic-response")]
-              subsonic_response: [<Actual $struct_name>],
-          }
-        }
-    };
-}
-
-pub(crate) use wrap_success_response_root;
+emit_constant_serialize!(open_subsonic_version, str, constant::OPEN_SUBSONIC_VERSION);
+emit_constant_serialize!(server_type, str, constant::SERVER_TYPE);
+emit_constant_serialize!(server_version, str, constant::SERVER_VERSION);
+emit_constant_serialize!(open_subsonic_support, bool, true);
+emit_constant_serialize!(status_ok, str, "ok");
+emit_constant_serialize!(status_failed, str, "failed");
