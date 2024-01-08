94011337bb17537ddf22c0f1c7980841cec72956
Vo Van Nghia <git@vnghia.com>
2024-01-08 23:21:14 +0700

    os(system): add handler suffix to handler

--- src/open_subsonic/system/ping.rs
+++ src/open_subsonic/system/ping.rs
@@ -6,6 +6,6 @@ use nghe_proc_macros::wrap_subsonic_response;
 #[derive(Debug, Default, Serialize)]
 pub struct PingResponse {}
 
-pub async fn ping() -> PingResponseJson {
+pub async fn ping_handler() -> PingResponseJson {
     PingResponse::default().into()
 }
