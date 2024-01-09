9d3aed32779b4351bd3dbd9f1230217f612a9fd3
Vo Van Nghia <git@vnghia.com>
2024-01-09 09:48:05 +0700

    os(user): add setup

--- src/open_subsonic/user/mod.rs
+++ src/open_subsonic/user/mod.rs
@@ -1,5 +1,6 @@
 pub mod create;
 pub mod password;
+pub mod setup;
 
 use axum::{routing::get, Router};
 
@@ -7,6 +8,7 @@ use crate::ServerState;
 
 pub fn router(server_state: ServerState) -> Router<ServerState> {
     Router::new()
+        .route("/rest/setup", get(setup::setup_handler))
         .route("/rest/createUser", get(create::create_user_handler))
         .with_state(server_state)
 }
---
+++ src/open_subsonic/user/setup.rs
@@ -0,0 +1,49 @@
+use super::super::OSResult;
+use super::create::{create_user, CreateUserParams};
+use crate::entity::*;
+use crate::{OpenSubsonicError, ServerState};
+
+use axum::extract::State;
+use axum::Form;
+use nghe_proc_macros::wrap_subsonic_response;
+use sea_orm::{EntityTrait, *};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Default, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SetupParams {
+    pub username: String,
+    pub password: String,
+    pub email: String,
+}
+
+#[wrap_subsonic_response]
+#[derive(Debug, Default, Serialize)]
+pub struct SetupBody {}
+
+#[axum::debug_handler]
+pub async fn setup_handler(
+    State(state): State<ServerState>,
+    Form(params): Form<SetupParams>,
+) -> OSResult<SetupResponse> {
+    if user::Entity::find().count(&state.conn).await? != 0 {
+        return Err(OpenSubsonicError::Forbidden {
+            message: Some("setup can only be used when there is no user".to_owned()),
+        });
+    }
+    create_user(
+        &state.conn,
+        &state.config.database.encryption_key,
+        CreateUserParams {
+            username: params.username,
+            password: params.password,
+            email: params.email,
+            admin_role: true,
+            download_role: true,
+            share_role: true,
+            ..Default::default()
+        },
+    )
+    .await?;
+    Ok(SetupBody::default().into())
+}
