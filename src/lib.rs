1aae03189921fad334a6ed431a6fe2433c72013d
Vo Van Nghia <git@vnghia.com>
2024-01-12 00:18:02 +0700

    malloc: use mimalloc instead of system malloc

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![deny(clippy::all)]
 
+use mimalloc::MiMalloc;
+
 pub mod config;
 pub mod entity;
 pub mod migrator;
@@ -10,3 +12,6 @@ pub mod utils;
 pub use migrator::Migrator;
 pub use open_subsonic::{OSResult, OpenSubsonicError};
 pub use state::ServerState;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
