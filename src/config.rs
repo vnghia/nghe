79f1f43dd65d5ef93fb76c666e58228b8e84e3a3
Vo Van Nghia <git@vnghia.com>
2024-01-13 16:02:15 +0700

    config: use hex deserializer for database encryption key

--- src/config.rs
+++ src/config.rs
@@ -4,7 +4,8 @@ mod built_info {
 
 use derivative::Derivative;
 use libaes::AES_128_KEY_LEN;
-use serde::{de::Error, Deserialize, Deserializer};
+use serde::Deserialize;
+use serde_with::serde_as;
 use std::{net::IpAddr, path::PathBuf};
 
 pub type EncryptionKey = [u8; AES_128_KEY_LEN];
@@ -16,6 +17,7 @@ pub struct Server {
     pub port: u16,
 }
 
+#[serde_as]
 #[derive(Derivative, Deserialize, Clone)]
 #[derivative(Debug)]
 #[allow(unused)]
@@ -23,22 +25,8 @@ pub struct Database {
     #[derivative(Debug = "ignore")]
     pub url: String,
     #[derivative(Debug = "ignore")]
-    #[serde(deserialize_with = "string_to_key")]
+    #[serde_as(as = "serde_with::hex::Hex")]
     pub encryption_key: EncryptionKey,
-}
-
-fn string_to_key<'de, D>(deserializer: D) -> Result<EncryptionKey, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let s: String = Deserialize::deserialize(deserializer)?;
-    if s.len() != AES_128_KEY_LEN {
-        Err(D::Error::custom(
-            "encryption key length should be 128-bit or 16 ascii character",
-        ))
-    } else {
-        s.as_bytes().try_into().map_err(D::Error::custom)
-    }
 }
 
 #[derive(Debug, Deserialize, Clone)]
