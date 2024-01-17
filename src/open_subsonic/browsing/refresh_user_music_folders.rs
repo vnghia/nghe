e804cd01d1805d736379729bd14e69e13e7b5f1b
Vo Van Nghia <git@vnghia.com>
2024-01-17 19:56:58 +0700

    os(browsing): add test setup helper

--- src/open_subsonic/browsing/refresh_user_music_folders.rs
+++ src/open_subsonic/browsing/refresh_user_music_folders.rs
@@ -42,55 +42,8 @@ pub async fn refresh_user_music_folders(
 
 #[cfg(test)]
 mod tests {
+    use super::super::test::setup_user_and_music_folders;
     use super::*;
-    use crate::utils::test::{db::TemporaryDatabase, fs::TemporaryFs, user::create_db_users};
-
-    async fn setup(
-        u1d1: bool,
-        u1d2: bool,
-        u2d1: bool,
-        u2d2: bool,
-    ) -> (
-        TemporaryDatabase,
-        TemporaryFs,
-        Vec<music_folder::Model>,
-        Vec<user_music_folder::Model>,
-    ) {
-        let (db, user_tokens) = create_db_users(2, 0).await;
-        let user1 = user_tokens[0].0.clone();
-        let user2 = user_tokens[1].0.clone();
-
-        let temp_fs = TemporaryFs::new();
-        let upserted_folders = temp_fs.create_music_folders(db.get_conn(), 2).await;
-
-        (
-            db,
-            temp_fs,
-            upserted_folders.clone(),
-            vec![
-                user_music_folder::Model {
-                    user_id: user1.id,
-                    music_folder_id: upserted_folders[0].id,
-                    allow: u1d1,
-                },
-                user_music_folder::Model {
-                    user_id: user1.id,
-                    music_folder_id: upserted_folders[1].id,
-                    allow: u1d2,
-                },
-                user_music_folder::Model {
-                    user_id: user2.id,
-                    music_folder_id: upserted_folders[0].id,
-                    allow: u2d1,
-                },
-                user_music_folder::Model {
-                    user_id: user2.id,
-                    music_folder_id: upserted_folders[1].id,
-                    allow: u2d2,
-                },
-            ],
-        )
-    }
 
     fn sort_models(mut models: Vec<user_music_folder::Model>) -> Vec<user_music_folder::Model> {
         models.sort_by_key(|model| model.user_id);
@@ -100,7 +53,8 @@ mod tests {
 
     #[tokio::test]
     async fn test_refresh_insert() {
-        let (db, _temp_fs, music_folders, permissions) = setup(true, true, true, true).await;
+        let (db, _temp_fs, music_folders, permissions) =
+            setup_user_and_music_folders(2, 2, &[true, true, true, true]).await;
         refresh_user_music_folders(db.get_conn(), &music_folders).await;
 
         let results = UserMusicFolder::find().all(db.get_conn()).await.unwrap();
@@ -113,7 +67,8 @@ mod tests {
 
     #[tokio::test]
     async fn test_refresh_do_nothing() {
-        let (db, _temp_fs, music_folders, permissions) = setup(true, false, true, true).await;
+        let (db, _temp_fs, music_folders, permissions) =
+            setup_user_and_music_folders(2, 2, &[true, false, true, true]).await;
         db.insert(permissions[1].clone().into_active_model()).await;
         db.insert(permissions[3].clone().into_active_model()).await;
         refresh_user_music_folders(db.get_conn(), &music_folders).await;
