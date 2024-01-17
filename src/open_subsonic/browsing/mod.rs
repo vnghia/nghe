1165ff2e32186ca43b1c3d919b041e8276b9bda9
Vo Van Nghia <git@vnghia.com>
2024-01-17 23:27:12 +0700

    os(browsing): add get music folders

--- src/open_subsonic/browsing/mod.rs
+++ src/open_subsonic/browsing/mod.rs
@@ -1,5 +1,22 @@
+pub mod get_music_folders;
 pub mod refresh_music_folders;
 pub mod refresh_user_music_folders;
 
 pub use refresh_music_folders::refresh_music_folders;
 pub use refresh_user_music_folders::refresh_user_music_folders;
+
+use axum::{routing::get, Router};
+
+use crate::ServerState;
+
+pub fn router(server_state: ServerState) -> Router<ServerState> {
+    Router::new()
+        .route(
+            "/rest/getMusicFolders",
+            get(get_music_folders::get_music_folders_handler),
+        )
+        .with_state(server_state)
+}
+
+#[cfg(test)]
+pub mod test;
