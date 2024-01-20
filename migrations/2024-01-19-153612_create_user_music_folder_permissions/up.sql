27de0f7bb4828f780046ebfc334e88b5de90e6c5
Vo Van Nghia <git@vnghia.com>
2024-01-20 22:42:47 +0700

    lint: sql migrations

--- migrations/2024-01-19-153612_create_user_music_folder_permissions/up.sql
+++ migrations/2024-01-19-153612_create_user_music_folder_permissions/up.sql
@@ -1,9 +1,10 @@
 -- Your SQL goes here
-create table user_music_folder_permissions (
-  user_id uuid not null,
-  music_folder_id uuid not null,
-  allow boolean not null default true,
-  constraint user_music_folder_permissions_pkey primary key(user_id, music_folder_id),
-  constraint user_music_folder_permissions_user_id_fkey foreign key(user_id) references users(id),
-  constraint user_music_folder_permissions_music_folder_id_fkey foreign key(music_folder_id) references music_folders(id)
-);
+create table
+  user_music_folder_permissions (
+    user_id uuid not null,
+    music_folder_id uuid not null,
+    allow boolean not null default true,
+    constraint user_music_folder_permissions_pkey primary key (user_id, music_folder_id),
+    constraint user_music_folder_permissions_user_id_fkey foreign key (user_id) references users (id),
+    constraint user_music_folder_permissions_music_folder_id_fkey foreign key (music_folder_id) references music_folders (id)
+  );
