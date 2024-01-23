6da1207f167896c211083fd80f48c1261a619795
Vo Van Nghia <git@vnghia.com>
2024-01-23 09:45:51 +0700

    put in a script

---
+++ ci/install-mold.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+MOLD_VERSION=2.4.0
+curl -L --retry 10 https://github.com/rui314/mold/releases/download/v${MOLD_VERSION}/mold-${MOLD_VERSION}-x86_64-linux.tar.gz | tar -C /usr/ --strip-components=1 --no-overwrite-dir -xzf -
+mold --version
