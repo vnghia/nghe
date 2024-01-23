ba0397a41d7b603819bc97f3e8be1df0d5540819
Vo Van Nghia <git@vnghia.com>
2024-01-23 13:23:53 +0700

    check

--- ci/cross-pre-build.sh
+++ ci/cross-pre-build.sh
@@ -8,3 +8,7 @@ fi
 
 MOLD_VERSION=2.4.0
 curl -L --retry 10 https://github.com/rui314/mold/releases/download/v${MOLD_VERSION}/mold-${MOLD_VERSION}-x86_64-linux.tar.gz | tar -C /usr/ --strip-components=1 --no-overwrite-dir -xzf -
+
+lsb_release -a
+
+gcc --version
