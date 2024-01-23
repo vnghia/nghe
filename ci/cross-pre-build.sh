246e42db3f76c0fa24c5fbaeab4b9528fd134343
Vo Van Nghia <git@vnghia.com>
2024-01-23 14:03:02 +0700

    only cargo

--- ci/cross-pre-build.sh
+++ ci/cross-pre-build.sh
@@ -5,10 +5,3 @@ if [[ "$CROSS_TARGET" == "aarch64-unknown-linux-gnu" ]]; then
   dpkg --add-architecture $CROSS_DEB_ARCH
   apt-get update && apt-get --assume-yes install libpq-dev:$CROSS_DEB_ARCH
 fi
-
-MOLD_VERSION=2.4.0
-curl -L --retry 10 https://github.com/rui314/mold/releases/download/v${MOLD_VERSION}/mold-${MOLD_VERSION}-x86_64-linux.tar.gz | tar -C /usr/ --strip-components=1 --no-overwrite-dir -xzf -
-
-apt-get update && apt-get --assume-yes install lsb-core && lsb_release -a
-
-gcc --version
