4020bdb690cbca41ce16a91672237e43a8605c20
Vo Van Nghia <git@vnghia.com>
2024-01-25 23:43:30 +0700

    test(os/scan): add test insert song

--- src/open_subsonic/scan/song.rs
+++ src/open_subsonic/scan/song.rs
@@ -38,7 +38,7 @@ pub async fn upsert_song<TI: AsRef<str>, TP: AsRef<Path>>(
     song_file_hash: u64,
     song_file_size: u64,
     song_relative_path: TP,
-) -> OSResult<()> {
+) -> OSResult<Uuid> {
     let artist_ids = upsert_artists(pool, ignored_prefixes, &song_tag.artists).await?;
     let album_id = upsert_album(pool, song_tag.album.into()).await?;
 
@@ -74,5 +74,52 @@ pub async fn upsert_song<TI: AsRef<str>, TP: AsRef<Path>>(
 
     upsert_song_artists(pool, song_id, &artist_ids).await?;
 
-    Ok(())
+    Ok(song_id)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        open_subsonic::browsing::test::setup_user_and_music_folders,
+        utils::test::song::query_all_song_information,
+    };
+
+    use fake::{Fake, Faker};
+    use itertools::Itertools;
+    use std::path::PathBuf;
+
+    #[tokio::test]
+    async fn test_upsert_song_insert() {
+        let (db, _, _, _temp_fs, music_folders, _) =
+            setup_user_and_music_folders(1, 1, &[true]).await;
+
+        let song_tag = Faker.fake::<SongTag>();
+        let song_path = Faker.fake::<PathBuf>();
+        let song_id = upsert_song(
+            db.get_pool(),
+            &[""],
+            music_folders[0].id,
+            None,
+            song_tag.clone(),
+            0,
+            0,
+            &song_path,
+        )
+        .await
+        .unwrap();
+
+        let (song, album, artists) = query_all_song_information(db.get_pool(), song_id).await;
+
+        assert_eq!(song_tag.title, song.title);
+        assert_eq!(song_tag.album, album.name);
+        assert_eq!(
+            song_tag.artists.into_iter().sorted().collect_vec(),
+            artists
+                .into_iter()
+                .map(|artist| artist.name)
+                .sorted()
+                .collect_vec()
+        );
+    }
 }
