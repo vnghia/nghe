0c72d3bc7f00a6812d0b9c3a3e9a7c77abbcdb6d
Vo Van Nghia <git@vnghia.com>
2024-01-26 15:06:17 +0700

    db: unique on (music_folder, hash, size) instead of (hash, size)

--- migrations/2024-01-21-014803_create_songs/up.sql
+++ migrations/2024-01-21-014803_create_songs/up.sql
@@ -13,7 +13,7 @@ create table
     constraint songs_album_id_fkey foreign key (album_id) references albums (id) on delete cascade,
     constraint songs_music_folder_id_path_key unique (music_folder_id, path),
     constraint songs_music_folder_id_fkey foreign key (music_folder_id) references music_folders (id) on delete cascade,
-    constraint songs_hash_size_key unique (file_hash, file_size)
+    constraint songs_music_folder_id_file_hash_file_size_key unique (music_folder_id, file_hash, file_size)
   );
 
 select
