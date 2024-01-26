ced387838ab7266cb8a07a1e9262a9663084228f
Vo Van Nghia <git@vnghia.com>
2024-01-26 23:36:22 +0700

    feat(utils/fs): use ignore to scan media files

--- src/utils/fs/files.rs
+++ src/utils/fs/files.rs
@@ -1,54 +1,73 @@
 use super::super::song::file_type::SONG_FILE_TYPES;
-use crate::OSResult;
+use crate::{
+    utils::song::file_type::{to_extension, to_glob_pattern},
+    OSResult,
+};
 
+use ignore::{types::TypesBuilder, WalkBuilder};
 use itertools::Itertools;
 use lofty::FileType;
 use std::path::{Path, PathBuf};
-use walkdir::WalkDir;
 
-pub fn scan_media_files<P: AsRef<Path>>(
+pub fn scan_media_files<P: AsRef<Path> + Clone + Send>(
     root: P,
 ) -> OSResult<Vec<(PathBuf, String, FileType, u64)>> {
-    Ok(WalkDir::new(&root)
-        .into_iter()
-        .filter_map(|entry| {
-            match entry {
-                Ok(entry) => {
-                    if let Some(extension) = entry.path().extension() {
-                        if let Some(file_type) = FileType::from_ext(extension) {
-                            if SONG_FILE_TYPES.contains(&file_type) {
-                                match entry.metadata() {
-                                    Ok(metadata) => {
-                                        if metadata.is_file() {
-                                            return Some(Ok((entry
-                                              .path().into(),
-                                                entry
-                                                    .path()
-                                                    .strip_prefix(&root)
-                                                    .expect(
-                                                        "this path should always contains the root path",
-                                                    )
-                                                    .to_string_lossy().to_string(),
-                                                file_type,
-                                                metadata.len(),
-                                            )));
-                                        }
-                                    }
-                                    Err(e) => {
-                                        return Some(Err(e));
-                                    }
-                                }
+    let (tx, rx) = crossbeam_channel::unbounded::<(PathBuf, String, FileType, u64)>();
+
+    let mut types = TypesBuilder::new();
+    for song_file_type in SONG_FILE_TYPES {
+        types.add(
+            to_extension(&song_file_type),
+            to_glob_pattern(&song_file_type),
+        )?;
+    }
+    let types = types.select("all").build()?;
+
+    WalkBuilder::new(&root)
+        .types(types)
+        .build_parallel()
+        .run(|| {
+            let tx = tx.clone();
+            let root = root.clone();
+            Box::new(move |entry| match entry {
+                Ok(entry) => match entry.metadata() {
+                    Ok(metadata) => {
+                        if metadata.is_file() {
+                            let entry_path = entry.path();
+                            if let Err(e) = tx.send((
+                                entry_path.into(),
+                                entry_path
+                                    .strip_prefix(&root)
+                                    .expect("this path should always contains the root path")
+                                    .to_string_lossy()
+                                    .to_string(),
+                                FileType::from_ext(
+                                    entry_path
+                                        .extension()
+                                        .expect("the file should have an extension"),
+                                )
+                                .expect("the extension should be covered in file type"),
+                                metadata.len(),
+                            )) {
+                                tracing::info!("error {} while scanning for media files", e);
                             }
                         }
+                        ignore::WalkState::Continue
                     }
-                }
+                    Err(e) => {
+                        tracing::info!("error {} while scanning for media files", e);
+                        ignore::WalkState::Continue
+                    }
+                },
                 Err(e) => {
-                    return Some(Err(e));
+                    tracing::info!("error {} while scanning for media files", e);
+                    ignore::WalkState::Continue
                 }
-            }
-            None
-        })
-        .try_collect()?)
+            })
+        });
+    drop(tx);
+
+    Ok(rx.into_iter().collect_vec())
 }
 
 #[cfg(test)]
--- src/utils/song/file_type.rs
+++ src/utils/song/file_type.rs
@@ -11,6 +11,14 @@ pub const fn to_extension(file_type: &FileType) -> &'static str {
     }
 }
 
+pub const fn to_glob_pattern(file_type: &FileType) -> &'static str {
+    match file_type {
+        FileType::Flac => "**/*.flac",
+        FileType::Mpeg => "**/*.mp3",
+        _ => "",
+    }
+}
+
 pub fn to_extensions() -> Vec<&'static str> {
     SONG_FILE_TYPES.iter().map(to_extension).collect_vec()
 }
