89f087c378af9cf0901d1176561f8b841a2988ee
Vo Van Nghia <git@vnghia.com>
2024-02-17 12:32:13 +0700

    db: add default value for updated at of configs

--- migrations/2024-02-16-174806_create_config/up.sql
+++ migrations/2024-02-16-174806_create_config/up.sql
@@ -4,7 +4,7 @@ create table
     key text not null constraint configs_pkey primary key,
     text text default null,
     byte bytea default null,
-    updated_at timestamptz not null
+    updated_at timestamptz not null default now()
   );
 
 select
