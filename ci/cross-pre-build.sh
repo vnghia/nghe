8b84649bf18abc0df1c8e90aeabc782f43887218
Vo Van Nghia <git@vnghia.com>
2024-03-14 14:28:43 +0100

    fix build on arm64 gnu

--- ci/cross-pre-build.sh
+++ ci/cross-pre-build.sh
@@ -3,5 +3,7 @@ set -euxo pipefail
 
 if [[ "$CROSS_TARGET" == "aarch64-unknown-linux-gnu" ]]; then
   dpkg --add-architecture $CROSS_DEB_ARCH
-  apt-get update && apt-get --assume-yes install libpq-dev:$CROSS_DEB_ARCH libclang-3.9-dev:$CROSS_DEB_ARCH clang-3.9:$CROSS_DEB_ARCH
+  apt-get update && apt-get --assume-yes install libpq-dev:$CROSS_DEB_ARCH \
+    libclang-3.9-dev clang-3.9 \
+    libclang-3.9-dev:$CROSS_DEB_ARCH clang-3.9:$CROSS_DEB_ARCH
 fi
