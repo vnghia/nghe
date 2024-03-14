cdc43ea1c1878527f5528c22425cc5d64a6877fb
Vo Van Nghia <git@vnghia.com>
2024-03-14 15:05:03 +0100

    use stable cross

--- ci/cross-pre-build.sh
+++ ci/cross-pre-build.sh
@@ -4,5 +4,5 @@ set -euxo pipefail
 if [[ "$CROSS_TARGET" == "aarch64-unknown-linux-gnu" ]]; then
   dpkg --add-architecture $CROSS_DEB_ARCH
   apt-get update && apt-get --assume-yes install libpq-dev:$CROSS_DEB_ARCH \
-    libclang-10-dev:$CROSS_DEB_ARCH clang-10:$CROSS_DEB_ARCH
+    libclang-3.9-dev:$CROSS_DEB_ARCH
 fi
