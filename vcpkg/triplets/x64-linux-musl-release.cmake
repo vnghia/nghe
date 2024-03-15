14ec9f37125ceaf41619aca5b6c8621b309cb60a
Vo Van Nghia <git@vnghia.com>
2024-03-15 18:04:56 +0100

    build musl x64 native on host

---
+++ vcpkg/triplets/x64-linux-musl-release.cmake
@@ -0,0 +1,2 @@
+include(${CMAKE_CURRENT_LIST_DIR}/x64-linux-musl.cmake)
+set(VCPKG_BUILD_TYPE "release")
---
+++ vcpkg/triplets/x64-linux-musl.cmake
@@ -0,0 +1,8 @@
+set(VCPKG_TARGET_ARCHITECTURE "x64")
+set(VCPKG_CRT_LINKAGE "dynamic")
+set(VCPKG_LIBRARY_LINKAGE "static")
+
+set(VCPKG_CMAKE_SYSTEM_NAME "Linux")
+
+set(CMAKE_CXX_COMPILER "musl-gcc")
+set(CMAKE_C_COMPILER "musl-gcc")
