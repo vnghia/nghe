7c8b570630ac4609d8889b20e04a3c34fb481272
Nghia <git@vnghia.com>
2024-03-15 20:08:40 +0100

    fix(transcode): switch to gstreamer (#51)
    * fix: switch to gstreamer
    
    * add alpine deps
    
    * build vcpkg before
    
    * add dummy deps
    
    * alpine libintl
    
    * use bison brew macos
    
    * build arm64 on host
    
    * fix pkg config path
    
    * print out log
    
    * fix osx pkg config path
    
    * print out log
    
    * print out log
    
    * remove x11
    
    * fix python error meson build
    
    * build windows using mingw
    
    * remove libx11
    
    * add path for x64 macos
    
    * macos build is good
    
    * build windows dynamic
    
    * purge nginx
    
    * purge nginx
    
    * vcpkg binary source global
    
    * build linux gnu ok
    
    * build windows ok
    
    * use gstreamer audio
    
    * install pkg-config
    
    * vcpkg cache for coverage
    
    * add new dependencies for windows
    
    * test windows
    
    * try build linux musl
    
    * add env
    
    * static c runtime on rust
    
    * build musl x64 native on host
    
    * fix pkg config path musl
    
    * build arm64 musl on host
    
    * test arm64 musl
    
    * test musl
    
    * test alpine
    
    * fix musl
    
    * fix musl
    
    * clean up docker file
    
    * print log
    
    * print out log
    
    * fix pkg config path
    
    * print out log
    
    * dont link static on windows
    
    * remove x11
    
    * leave window the same
    
    * build linux ok
    
    * build windows only
    
    * ls lah
    
    * windows
    
    * windows
    
    * cat
    
    * test
    
    * bash
    
    * shell
    
    * export
    
    * skip windows
    
    * add gstreamer audio sys

--- vcpkg/triplets/arm64-linux-musl-release.cmake
+++ vcpkg/triplets/arm64-linux-musl-release.cmake
@@ -1,3 +1,9 @@
-include(${CMAKE_CURRENT_LIST_DIR}/arm64-linux-musl.cmake)
-set(VCPKG_CRT_LINKAGE "static")
+set(VCPKG_TARGET_ARCHITECTURE "arm64")
+set(VCPKG_CRT_LINKAGE "dynamic")
+set(VCPKG_LIBRARY_LINKAGE "static")
 set(VCPKG_BUILD_TYPE "release")
+
+set(VCPKG_CMAKE_SYSTEM_NAME "Linux")
+
+set(CMAKE_CXX_COMPILER "aarch64-linux-musl-g++")
+set(CMAKE_C_COMPILER "aarch64-linux-musl-gcc")
--- vcpkg/triplets/arm64-linux-musl.cmake
+++
@@ -1,9 +0,0 @@
-set(VCPKG_TARGET_ARCHITECTURE "arm64")
-set(VCPKG_CRT_LINKAGE "dynamic")
-set(VCPKG_LIBRARY_LINKAGE "static")
-
-set(VCPKG_CMAKE_SYSTEM_NAME "Linux")
-
-set(CMAKE_CXX_COMPILER "aarch64-alpine-linux-musl-c++")
-set(CMAKE_C_COMPILER "aarch64-alpine-linux-musl-cc")
-set(CMAKE_HOST_SYSTEM_PROCESSOR "aarch64")
