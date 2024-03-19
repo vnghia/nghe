5967ae1636043a5c8ba579c599274b1bd8a8538d
Vo Van Nghia <git@vnghia.com>
2024-03-19 10:47:06 +0100

    fix(axum): add graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod built_info {
 use axum::Router;
 use itertools::Itertools;
 use nghe::models::music_folders;
+use tokio::signal;
 use tower::ServiceBuilder;
 use tower_http::trace::TraceLayer;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
@@ -78,6 +79,7 @@ async fn main() {
         .unwrap();
     tracing::info!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app(database, upserted_music_folders))
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
 }
@@ -95,4 +97,28 @@ fn app(database: Database, music_folders: Vec<music_folders::MusicFolder>) -> Ro
         // layer
         .layer(ServiceBuilder::new().layer(TraceLayer::new_for_http()))
         .with_state(database)
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
 }
