8434fd794207b0e92530956bd39773968c21287d
Vo Van Nghia <git@vnghia.com>
2024-03-22 15:32:40 +0100

    fix: change version type and add version commit

--- src/open_subsonic/common/constant.rs
+++ src/open_subsonic/common/constant.rs
@@ -3,5 +3,10 @@ mod built_info {
 }
 
 pub const OPEN_SUBSONIC_VERSION: &str = "v1.16.1";
-pub const SERVER_TYPE: &str = "Nghe";
-pub const SERVER_VERSION: &str = built_info::PKG_VERSION;
+pub const SERVER_TYPE: &str = built_info::PKG_NAME;
+pub const SERVER_VERSION: &str = constcat::concat!(
+    built_info::PKG_VERSION,
+    " (",
+    crate::utils::constu::unwrap(built_info::GIT_COMMIT_HASH_SHORT),
+    ")"
+);
