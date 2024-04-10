832c91fd2cfd9b16e819183bf186b12156eb8ce2
Vo Van Nghia <git@vnghia.com>
2024-04-10 19:26:10 +0200

    fix(constant): remove v prefix in subsonic version

--- types/src/common/constant.rs
+++ types/src/common/constant.rs
@@ -2,7 +2,7 @@ mod built_info {
     include!(concat!(env!("OUT_DIR"), "/built.rs"));
 }
 
-pub const OPEN_SUBSONIC_VERSION: &str = "v1.16.1";
+pub const OPEN_SUBSONIC_VERSION: &str = "1.16.1";
 pub const SERVER_NAME: &str = "nghe";
 pub const SERVER_VERSION: &str = constcat::concat!(
     built_info::PKG_VERSION,
