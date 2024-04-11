080593f6e3d8661f483d7e49b088d8afd4a4596f
Vo Van Nghia <git@vnghia.com>
2024-04-11 08:31:51 +0200

    feat(frontend): add logout button

--- frontend/src/components/drawer/mod.rs
+++ frontend/src/components/drawer/mod.rs
@@ -1,9 +1,16 @@
 use dioxus::prelude::*;
 
+use crate::state::use_common_state;
 use crate::Route;
 
 #[component]
 pub fn Drawer() -> Element {
+    let mut common_state = use_common_state();
+
+    let onclick_logout = move |_: Event<MouseData>| {
+        common_state.set(None);
+    };
+
     rsx! {
         div { class: "drawer lg:drawer-open",
             input {
@@ -12,23 +19,21 @@ pub fn Drawer() -> Element {
                 id: "main-drawer-toggle"
             }
             div { class: "drawer-content flex flex-col",
-                div { class: "w-full navbar bg-base-300",
+                div { class: "w-full navbar shadow bg-base-300",
                     div { class: "navbar-start",
                         div { class: "flex-none lg:hidden",
                             label {
-                                "aria-label": "open sidebar",
+                                aria_label: "open sidebar",
                                 r#for: "main-drawer-toggle",
                                 class: "btn btn-square btn-ghost",
                                 svg {
-                                    "viewBox": "0 0 24 24",
-                                    "xmlns": "http://www.w3.org/2000/svg",
-                                    "fill": "none",
-                                    class: "inline-block w-6 h-6 stroke-current",
+                                    view_box: "fill-none 0 0 24 24",
+                                    xmlns: "http://www.w3.org/2000/svg",
+                                    class: "inline-block w-6 h-6 stroke-current stroke-2",
                                     path {
-                                        "stroke-linecap": "round",
-                                        "stroke-width": "2",
-                                        "stroke-linejoin": "round",
-                                        "d": "M4 6h16M4 12h16M4 18h16"
+                                        stroke_linecap: "round",
+                                        stroke_linejoin: "round",
+                                        d: "M4 6h16M4 12h16M4 18h16"
                                     }
                                 }
                             }
@@ -40,16 +45,38 @@ pub fn Drawer() -> Element {
                     div { class: "navbar-end",
                         button { class: "btn btn-ghost btn-circle",
                             svg {
-                                "xmlns": "http://www.w3.org/2000/svg",
-                                "fill": "none",
-                                "stroke": "currentColor",
-                                "viewBox": "0 0 24 24",
-                                class: "h-5 w-5",
+                                class: "fill-none w-6 h-6 stroke-base-content stroke-2",
+                                xmlns: "http://www.w3.org/2000/svg",
+                                view_box: "0 0 24 24",
                                 path {
-                                    "stroke-linecap": "round",
-                                    "d": "M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z",
-                                    "stroke-linejoin": "round",
-                                    "stroke-width": "2"
+                                    stroke_linecap: "round",
+                                    stroke_linejoin: "round",
+                                    d: "M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                                }
+                            }
+                        }
+                        div { class: "dropdown dropdown-end",
+                            div {
+                                class: "btn btn-ghost btn-circle flex justify-center items-center",
+                                tabindex: "0",
+                                role: "button",
+                                svg {
+                                    class: "fill-none w-8 h-8 stroke-base-content",
+                                    xmlns: "http://www.w3.org/2000/svg",
+                                    view_box: "0 0 24 24",
+                                    stroke_width: "1.5",
+                                    path {
+                                        stroke_linejoin: "round",
+                                        stroke_linecap: "round",
+                                        d: "M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                                    }
+                                }
+                            }
+                            ul {
+                                tabindex: "0",
+                                class: "mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-300 rounded-box w-52",
+                                li {
+                                    button { onclick: onclick_logout, "Logout" }
                                 }
                             }
                         }
@@ -60,17 +87,10 @@ pub fn Drawer() -> Element {
             div { class: "drawer-side",
                 label {
                     r#for: "main-drawer-toggle",
-                    "aria-label": "close sidebar",
+                    aria_label: "close sidebar",
                     class: "drawer-overlay"
                 }
-                ul { class: "menu p-4 w-80 min-h-full bg-base-200 text-base-content",
-                    li {
-                        a { "Sidebar Item 1" }
-                    }
-                    li {
-                        a { "Sidebar Item 2" }
-                    }
-                }
+                ul { class: "menu p-4 w-80 min-h-full bg-base-200 text-base-content" }
             }
         }
     }
