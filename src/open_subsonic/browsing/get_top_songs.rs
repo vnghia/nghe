705bbe3ed75314241f3b6945eb1d7ab5ec837302
Vo Van Nghia <git@vnghia.com>
2024-04-13 17:12:41 +0200

    fix(browsing): get top songs return instance level top songs

--- src/open_subsonic/browsing/get_top_songs.rs
+++ src/open_subsonic/browsing/get_top_songs.rs
@@ -5,7 +5,6 @@ use diesel::{ExpressionMethods, JoinOnDsl, PgSortExpressionMethods, QueryDsl};
 use diesel_async::RunQueryDsl;
 use futures::{stream, StreamExt, TryStreamExt};
 use nghe_proc_macros::{add_axum_response, add_common_validate};
-use uuid::Uuid;
 
 use crate::models::*;
 use crate::open_subsonic::id3::*;
@@ -16,14 +15,12 @@ add_axum_response!(GetTopSongsBody);
 
 async fn get_top_songs(
     pool: &DatabasePool,
-    user_id: Uuid,
     artist: String,
     count: Option<u32>,
 ) -> Result<Vec<SongId3Db>> {
     get_song_id3_db()
         .inner_join(artists::table.on(artists::id.eq(songs_artists::artist_id)))
         .inner_join(playbacks::table)
-        .filter(playbacks::user_id.eq(user_id))
         .filter(artists::name.eq(artist))
         .order(sum(playbacks::count).desc().nulls_last())
         .limit(count.unwrap_or(50) as _)
@@ -40,12 +37,10 @@ pub async fn get_top_songs_handler(
     Ok(axum::Json(
         GetTopSongsBody {
             top_songs: TopSongs {
-                song: stream::iter(
-                    get_top_songs(pool, req.user_id, req.params.artist, req.params.count).await?,
-                )
-                .then(|v| async move { v.into(pool).await })
-                .try_collect()
-                .await?,
+                song: stream::iter(get_top_songs(pool, req.params.artist, req.params.count).await?)
+                    .then(|v| async move { v.into(pool).await })
+                    .try_collect()
+                    .await?,
             },
         }
         .into(),
