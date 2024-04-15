9886204bcbcafa93cc56d243a7fddcad5efe0239
Nghia <git@vnghia.com>
2024-04-15 17:07:05 +0200

    fix(music-folder): get music folder stats for empty folder (#102)

--- src/open_subsonic/common/sql.rs
+++ src/open_subsonic/common/sql.rs
@@ -1,3 +1,6 @@
 use diesel::sql_function;
+use diesel::sql_types::*;
 
 sql_function!(fn random() -> Text);
+
+sql_function!(fn coalesce(x: Nullable<Int8>, y: Nullable<Int8>) -> Int8);
--- src/open_subsonic/music_folder/get_music_folder_stats.rs
+++ src/open_subsonic/music_folder/get_music_folder_stats.rs
@@ -2,13 +2,14 @@ use anyhow::Result;
 use axum::extract::State;
 use diesel::dsl::{count, count_distinct, sum};
 use diesel::{
-    ExpressionMethods, JoinOnDsl, NullableExpressionMethods, QueryDsl, Queryable, SelectableHelper,
+    BoolExpressionMethods, ExpressionMethods, JoinOnDsl, NullableExpressionMethods, QueryDsl,
+    Queryable, SelectableHelper,
 };
 use diesel_async::RunQueryDsl;
 use nghe_proc_macros::{add_axum_response, add_common_validate, add_convert_types};
 
 use crate::models::*;
-use crate::open_subsonic::id3::*;
+use crate::open_subsonic::sql::coalesce;
 use crate::{Database, DatabasePool};
 
 add_common_validate!(GetMusicFolderStatsParams, admin);
@@ -30,25 +31,40 @@ struct MusicFolderStat {
 }
 
 async fn get_music_folder_stats(pool: &DatabasePool) -> Result<Vec<MusicFolderStat>> {
-    let songs_total_size = diesel::alias!(songs as songs_total_size);
-
-    get_basic_artist_id3_db()
-        .inner_join(music_folders::table.on(music_folders::id.eq(songs::music_folder_id)))
-        .group_by(music_folders::id)
+    music_folders::table
         .select((
             music_folders::MusicFolder::as_select(),
-            count_distinct(artists::id),
-            count_distinct(songs::album_id),
-            count_distinct(songs::id),
+            artists::table
+                .left_join(songs_album_artists::table)
+                .left_join(songs_artists::table)
+                .inner_join(
+                    songs::table.on(songs::id
+                        .eq(songs_album_artists::song_id)
+                        .or(songs::id.eq(songs_artists::song_id))),
+                )
+                .filter(songs::music_folder_id.eq(music_folders::id))
+                .select(count_distinct(artists::id))
+                .single_value()
+                .assume_not_null(),
+            songs::table
+                .filter(songs::music_folder_id.eq(music_folders::id))
+                .select(count_distinct(songs::album_id))
+                .single_value()
+                .assume_not_null(),
+            songs::table
+                .filter(songs::music_folder_id.eq(music_folders::id))
+                .select(count(songs::id))
+                .single_value()
+                .assume_not_null(),
             user_music_folder_permissions::table
                 .filter(user_music_folder_permissions::music_folder_id.eq(music_folders::id))
                 .filter(user_music_folder_permissions::allow)
                 .select(count(user_music_folder_permissions::user_id))
                 .single_value()
                 .assume_not_null(),
-            songs_total_size
-                .filter(songs_total_size.field(songs::music_folder_id).eq(music_folders::id))
-                .select(sum(songs_total_size.field(songs::file_size)))
+            songs::table
+                .filter(songs::music_folder_id.eq(music_folders::id))
+                .select(coalesce(sum(songs::file_size), 0))
                 .single_value()
                 .assume_not_null(),
         ))
@@ -91,29 +107,58 @@ mod tests {
 
         let mut infra =
             Infra::new().await.n_folder(n_folder).await.add_user(None).await.add_user(None).await;
+
+        let folder_stats = get_music_folder_stats(infra.pool())
+            .await
+            .unwrap()
+            .into_iter()
+            .sorted_by_key(|s| s.music_folder.id)
+            .collect_vec();
+        let folder_fs_stats = infra
+            .music_folders
+            .iter()
+            .cloned()
+            .map(|music_folder| MusicFolderStat {
+                music_folder,
+                artist_count: 0,
+                album_count: 0,
+                song_count: 0,
+                user_count: 2,
+                total_size: 0,
+            })
+            .sorted_by_key(|s| s.music_folder.id)
+            .collect_vec();
+        assert_eq!(folder_stats, folder_fs_stats);
+
         infra.permissions(.., ..5, false).await;
 
         (0..n_folder).for_each(|i| {
-            let n_song = (10..20).fake();
-            infra.add_songs(
-                i,
-                (0..n_song)
-                    .map(|_| SongTag {
-                        artists: artists
-                            .choose_multiple(&mut rand::thread_rng(), (1..2).fake())
-                            .cloned()
-                            .map(String::into)
-                            .collect(),
-                        album_artists: artists
-                            .choose_multiple(&mut rand::thread_rng(), (1..2).fake())
-                            .cloned()
-                            .map(String::into)
-                            .collect(),
-                        album: album.choose(&mut rand::thread_rng()).unwrap().to_string().into(),
-                        ..Faker.fake()
-                    })
-                    .collect(),
-            );
+            if i < n_folder - 1 {
+                let n_song = (10..20).fake();
+                infra.add_songs(
+                    i,
+                    (0..n_song)
+                        .map(|_| SongTag {
+                            artists: artists
+                                .choose_multiple(&mut rand::thread_rng(), (1..2).fake())
+                                .cloned()
+                                .map(String::into)
+                                .collect(),
+                            album_artists: artists
+                                .choose_multiple(&mut rand::thread_rng(), (1..2).fake())
+                                .cloned()
+                                .map(String::into)
+                                .collect(),
+                            album: album
+                                .choose(&mut rand::thread_rng())
+                                .unwrap()
+                                .to_string()
+                                .into(),
+                            ..Faker.fake()
+                        })
+                        .collect(),
+                );
+            }
         });
         infra.scan(.., None).await;
 
@@ -129,22 +174,34 @@ mod tests {
             .cloned()
             .enumerate()
             .map(|(i, music_folder)| {
-                let slice = i..=i;
+                if i < n_folder - 1 {
+                    let slice = i..=i;
 
-                let artist_count = infra.artist_no_ids(slice.clone()).len() as _;
-                let album_count = infra.album_no_ids(slice.clone()).len() as _;
+                    let artist_count = infra.artist_no_ids(slice.clone()).len() as _;
+                    let album_count = infra.album_no_ids(slice.clone()).len() as _;
 
-                let song_fs_infos = infra.song_fs_infos(slice.clone());
-                let song_count = song_fs_infos.len() as _;
-                let total_size = song_fs_infos.iter().fold(0_u32, |aac, s| aac + s.file_size) as _;
+                    let song_fs_infos = infra.song_fs_infos(slice.clone());
+                    let song_count = song_fs_infos.len() as _;
+                    let total_size =
+                        song_fs_infos.iter().fold(0_u32, |aac, s| aac + s.file_size) as _;
 
-                MusicFolderStat {
-                    music_folder,
-                    artist_count,
-                    album_count,
-                    song_count,
-                    user_count: if i < 5 { 0 } else { 2 },
-                    total_size,
+                    MusicFolderStat {
+                        music_folder,
+                        artist_count,
+                        album_count,
+                        song_count,
+                        user_count: if i < 5 { 0 } else { 2 },
+                        total_size,
+                    }
+                } else {
+                    MusicFolderStat {
+                        music_folder,
+                        artist_count: 0,
+                        album_count: 0,
+                        song_count: 0,
+                        user_count: 2,
+                        total_size: 0,
+                    }
                 }
             })
             .sorted_by_key(|s| s.music_folder.id)
