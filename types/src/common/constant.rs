d578f1db979e24a5cb7a5ec420ec1d1a49c30445
Vo Van Nghia <git@vnghia.com>
2024-04-17 15:55:25 +0200

    git commit hash

--- types/src/common/constant.rs
+++ types/src/common/constant.rs
@@ -2,11 +2,15 @@ mod built_info {
     include!(concat!(env!("OUT_DIR"), "/built.rs"));
 }
 
+const fn git_commit_hash_short() -> &'static str {
+    if built_info::GIT_COMMIT_HASH_SHORT.is_some() {
+        built_info::GIT_COMMIT_HASH_SHORT.unwrap()
+    } else {
+        "0"
+    }
+}
+
 pub const OPEN_SUBSONIC_VERSION: &str = "1.16.1";
 pub const SERVER_NAME: &str = "nghe";
-pub const SERVER_VERSION: &str = constcat::concat!(
-    built_info::PKG_VERSION,
-    " (",
-    built_info::GIT_COMMIT_HASH_SHORT.unwrap(),
-    ")"
-);
+pub const SERVER_VERSION: &str =
+    constcat::concat!(built_info::PKG_VERSION, " (", git_commit_hash_short(), ")");
