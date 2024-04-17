742e965f7d42943479bc3887f204515a0aff9aac
Vo Van Nghia <git@vnghia.com>
2024-04-17 02:48:02 +0200

    fix cross

--- ci/cross-pre-build.sh
+++ ci/cross-pre-build.sh
@@ -5,4 +5,9 @@ if [[ "$CROSS_TARGET" == "aarch64-unknown-linux-gnu" ]]; then
   dpkg --add-architecture $CROSS_DEB_ARCH
   apt-get update && apt-get --assume-yes install libpq-dev:$CROSS_DEB_ARCH \
     libclang-10-dev
+elif [[ "$CROSS_TARGET" == "x86_64-unknown-freebsd" ]]; then
+  # Issue: https://github.com/cross-rs/cross/issues/1367
+  mkdir -p /usr/local/x86_64-unknown-freebsd12/usr &&
+    ln -s ../include /usr/local/x86_64-unknown-freebsd12/usr/include &&
+    ln -s ../lib /usr/local/x86_64-unknown-freebsd12/usr/lib
 fi
