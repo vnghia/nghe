506cc2c0326934fac431cb4debf83f3217115a34
Vo Van Nghia <git@vnghia.com>
2024-04-18 14:28:25 +0200

    fix(deps): update rust crate dioxus-logger to 0.5.0

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,13 +1,12 @@
 #![allow(non_snake_case)]
 
 use dioxus::prelude::*;
-use log::LevelFilter;
 
 const _TAILWIND_STYLE: &str = manganis::mg!(file("public/tailwind.css"));
 
 fn main() {
     // Init debug
-    dioxus_logger::init(LevelFilter::Info).expect("failed to init logger");
+    dioxus_logger::init(tracing::Level::INFO).expect("failed to init logger");
     console_error_panic_hook::set_once();
 
     launch(App);
