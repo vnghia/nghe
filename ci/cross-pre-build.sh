470bf73719a1403d2b336af6ca7173512ce7d8aa
Nghia <git@vnghia.com>
2024-04-18 15:48:13 +0200

    ci(linux-aarch64): disable postgres deps in test (#128)

--- ci/cross-pre-build.sh
+++ ci/cross-pre-build.sh
@@ -2,7 +2,5 @@
 set -euxo pipefail
 
 if [[ "$CROSS_TARGET" == "aarch64-unknown-linux-gnu" ]]; then
-  dpkg --add-architecture $CROSS_DEB_ARCH
-  apt-get update && apt-get --assume-yes install libpq-dev:$CROSS_DEB_ARCH \
-    libclang-10-dev
+  apt-get update && apt-get --assume-yes install libclang-10-dev
 fi
