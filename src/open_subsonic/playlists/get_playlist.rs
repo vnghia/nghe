712eb5c3bc17ee368fc1f7a49c689071ff43514b
Vo Van Nghia <git@vnghia.com>
2024-04-19 23:12:21 +0200

    tests(os/playlists): add test for get playlist

--- src/open_subsonic/playlists/get_playlist.rs
+++ src/open_subsonic/playlists/get_playlist.rs
@@ -32,3 +32,82 @@ pub async fn get_playlist_handler(
         .into(),
     ))
 }
+
+#[cfg(test)]
+mod tests {
+    use nghe_types::playlists::create_playlist::CreatePlaylistParams;
+    use rand::prelude::SliceRandom;
+
+    use super::super::create_playlist::create_playlist;
+    use super::*;
+    use crate::utils::test::Infra;
+
+    #[tokio::test]
+    async fn test_get_playlist() {
+        let n_song = 10_usize;
+        let playlist_name = "playlist";
+
+        let mut infra = Infra::new().await.add_user(None).await.add_folder(true).await;
+        infra.add_n_song(0, n_song).scan(.., None).await;
+        let mut song_fs_ids = infra.song_ids(..).await;
+        song_fs_ids.shuffle(&mut rand::thread_rng());
+
+        let playlist_id = create_playlist(
+            infra.pool(),
+            infra.user_id(0),
+            &CreatePlaylistParams {
+                name: Some(playlist_name.into()),
+                playlist_id: None,
+                song_ids: song_fs_ids.clone(),
+            },
+        )
+        .await
+        .unwrap()
+        .playlist
+        .basic
+        .id;
+
+        let PlaylistId3WithSongIdsDb { playlist, song_ids } =
+            get_playlist(infra.pool(), infra.user_id(0), playlist_id).await.unwrap();
+
+        assert_eq!(playlist.basic.name, playlist_name);
+        assert!(!playlist.basic.public);
+        assert_eq!(playlist.song_count, song_ids.len() as i64);
+
+        assert_eq!(song_fs_ids, song_ids);
+    }
+
+    #[tokio::test]
+    async fn test_get_playlist_partial() {
+        let n_song = 10_usize;
+        let playlist_name = "playlist";
+
+        let mut infra = Infra::new().await.add_user(None).await.n_folder(2).await;
+        infra.add_n_song(0, n_song).add_n_song(1, n_song).scan(.., None).await;
+        infra.remove_permission(None, 1).await;
+
+        let playlist_id = create_playlist(
+            infra.pool(),
+            infra.user_id(0),
+            &CreatePlaylistParams {
+                name: Some(playlist_name.into()),
+                playlist_id: None,
+                song_ids: infra.song_ids(..).await,
+            },
+        )
+        .await
+        .unwrap()
+        .playlist
+        .basic
+        .id;
+
+        let PlaylistId3WithSongIdsDb { playlist, song_ids } =
+            get_playlist(infra.pool(), infra.user_id(0), playlist_id).await.unwrap();
+
+        assert_eq!(playlist.basic.name, playlist_name);
+        assert!(!playlist.basic.public);
+        assert_eq!(playlist.song_count, song_ids.len() as i64);
+
+        assert_eq!(infra.song_ids(..1).await, song_ids);
+    }
+}
