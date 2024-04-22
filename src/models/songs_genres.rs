2c8e487c352479bb7110ddc910725465093f05d2
Nghia <git@vnghia.com>
2024-04-22 10:45:06 +0200

    fix(diesel): add check for backend for all struct (#151)

--- src/models/songs_genres.rs
+++ src/models/songs_genres.rs
@@ -6,6 +6,7 @@ pub use crate::schema::songs_genres;
 
 #[derive(Insertable)]
 #[diesel(table_name = songs_genres)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct NewSongGenre {
     pub song_id: Uuid,
     pub genre_id: Uuid,
--- src/models/user_music_folder_permissions.rs
+++ src/models/user_music_folder_permissions.rs
@@ -6,6 +6,7 @@ pub use crate::schema::user_music_folder_permissions;
 
 #[derive(Insertable)]
 #[diesel(table_name = user_music_folder_permissions)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct NewUserMusicFolderPermission {
     pub user_id: Uuid,
     pub music_folder_id: Uuid,
