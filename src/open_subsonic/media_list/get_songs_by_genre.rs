911cecb475d6d6b4fcf114019ebb8f415cb480dd
Vo Van Nghia <git@vnghia.com>
2024-04-22 12:30:41 +0200

    feat(os/media-list): add get songs by genre

---
+++ src/open_subsonic/media_list/get_songs_by_genre.rs
@@ -0,0 +1,100 @@
+use anyhow::Result;
+use axum::extract::State;
+use diesel::{ExpressionMethods, QueryDsl};
+use diesel_async::RunQueryDsl;
+use futures::{stream, StreamExt, TryStreamExt};
+use nghe_proc_macros::{
+    add_axum_response, add_common_validate, add_count_offset, add_permission_filter,
+};
+use uuid::Uuid;
+
+use crate::models::*;
+use crate::open_subsonic::id3::*;
+use crate::open_subsonic::permission::check_permission;
+use crate::{Database, DatabasePool};
+
+add_common_validate!(GetSongsByGenreParams);
+add_axum_response!(GetSongsByGenreBody);
+
+async fn get_songs_by_genre(
+    pool: &DatabasePool,
+    user_id: Uuid,
+    params: GetSongsByGenreParams,
+) -> Result<Vec<SongId3Db>> {
+    let GetSongsByGenreParams { genre, count, offset, music_folder_ids } = params;
+
+    check_permission(pool, user_id, &music_folder_ids).await?;
+
+    #[add_permission_filter]
+    #[add_count_offset]
+    get_song_id3_db()
+        .order(songs::title.asc())
+        .filter(genres::value.eq(genre))
+        .get_results(&mut pool.get().await?)
+        .await
+        .map_err(anyhow::Error::from)
+}
+
+pub async fn get_songs_by_genre_handler(
+    State(database): State<Database>,
+    req: GetSongsByGenreRequest,
+) -> GetSongsByGenreJsonResponse {
+    let pool = &database.pool;
+    Ok(axum::Json(
+        GetSongsByGenreBody {
+            songs_by_genre: SongsByGenre {
+                song: stream::iter(get_songs_by_genre(pool, req.user_id, req.params).await?)
+                    .then(|v| async move { v.into(pool).await })
+                    .try_collect()
+                    .await?,
+            },
+        }
+        .into(),
+    ))
+}
+
+#[cfg(test)]
+mod tests {
+    use fake::{Fake, Faker};
+
+    use super::*;
+    use crate::utils::song::test::SongTag;
+    use crate::utils::test::Infra;
+
+    #[tokio::test]
+    async fn test_get_songs_by_genre() {
+        let genre_values = ["genre1", "genre2"];
+        let n_song = 10_usize;
+        let n_diff = 3_usize;
+        let mut infra = Infra::new().await.n_folder(1).await.add_user(None).await;
+        infra
+            .add_songs(
+                0,
+                (0..n_song + n_diff)
+                    .map(|_| SongTag { genres: vec![genre_values[0].into()], ..Faker.fake() })
+                    .collect(),
+            )
+            .add_songs(
+                0,
+                (0..n_song - n_diff)
+                    .map(|_| SongTag { genres: vec![genre_values[1].into()], ..Faker.fake() })
+                    .collect(),
+            )
+            .scan(.., None)
+            .await;
+
+        let songs = get_songs_by_genre(
+            infra.pool(),
+            infra.user_id(0),
+            GetSongsByGenreParams {
+                genre: genre_values[0].to_string(),
+                count: None,
+                offset: None,
+                music_folder_ids: None,
+            },
+        )
+        .await
+        .unwrap();
+        assert_eq!(songs.len(), n_song + n_diff);
+    }
+}
--- src/open_subsonic/media_list/mod.rs
+++ src/open_subsonic/media_list/mod.rs
@@ -1,7 +1,13 @@
 mod get_album_list2;
 mod get_random_songs;
+mod get_songs_by_genre;
 mod get_starred2;
 
 pub fn router() -> axum::Router<crate::Database> {
-    nghe_proc_macros::build_router!(get_starred2, get_album_list2, get_random_songs)
+    nghe_proc_macros::build_router!(
+        get_starred2,
+        get_album_list2,
+        get_random_songs,
+        get_songs_by_genre
+    )
 }
