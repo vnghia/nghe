3b8cc42e649cb5ce4b40be5214d29e5cdfeb1bfa
Vo Van Nghia <git@vnghia.com>
2024-04-23 15:58:15 +0200

    fix(freebsd): increase sleep time for freebsd to 100 microseconds

--- src/utils/test/infra.rs
+++ src/utils/test/infra.rs
@@ -166,7 +166,11 @@ impl Infra {
 
         // Postgres timestamp resolution is microsecond.
         // So we wait for 10 microseconds to make sure that there is no overlap scans.
-        tokio::time::sleep(std::time::Duration::from_micros(10)).await;
+        if cfg!(target_os = "freebsd") {
+            tokio::time::sleep(std::time::Duration::from_micros(100)).await;
+        } else {
+            tokio::time::sleep(std::time::Duration::from_micros(10)).await;
+        }
         result
     }
 
