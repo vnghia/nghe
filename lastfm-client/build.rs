326e87cd5e4f36aaa829113777ddbbd384544c03
Vo Van Nghia <git@vnghia.com>
2024-04-24 18:26:24 +0200

    check env empty

--- lastfm-client/build.rs
+++ lastfm-client/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    if std::env::var("LASTFM_KEY").is_ok() {
+    if std::env::var("LASTFM_KEY").is_ok_and(|s| !s.is_empty()) {
         println!("cargo::rustc-cfg=lastfm_env")
     }
 }
