840e5235913fb58c217e01601132ef223d1b23e5
Vo Van Nghia <git@vnghia.com>
2024-05-06 15:39:55 +0200

    feat(os): sorted results in album and playlist

--- src/open_subsonic/browsing/get_album.rs
+++ src/open_subsonic/browsing/get_album.rs
@@ -4,6 +4,7 @@ use diesel::dsl::sql;
 use diesel::{sql_types, ExpressionMethods, OptionalExtension, QueryDsl, SelectableHelper};
 use diesel_async::RunQueryDsl;
 use futures::{stream, StreamExt, TryStreamExt};
+use itertools::Itertools;
 use nghe_proc_macros::{add_axum_response, add_common_validate};
 use uuid::Uuid;
 
@@ -21,17 +22,23 @@ async fn get_album_and_song_ids(
     user_id: Uuid,
     album_id: Uuid,
 ) -> Result<(AlbumId3Db, Vec<Uuid>)> {
-    get_album_id3_db()
+    let (album, song_ids) = get_album_id3_db()
         .filter(with_permission(user_id))
         .filter(albums::id.eq(album_id))
         .select((
             AlbumId3Db::as_select(),
-            sql::<sql_types::Array<sql_types::Uuid>>("array_agg(distinct(songs.id)) song_ids"),
+            sql::<sql_types::Array<sql_types::Uuid>>(
+                "array_agg(songs.id order by songs.disc_number asc nulls last, songs.track_number \
+                 asc nulls last) song_ids",
+            ),
         ))
         .first::<(AlbumId3Db, Vec<Uuid>)>(&mut pool.get().await?)
         .await
         .optional()?
-        .ok_or_else(|| OSError::NotFound("Album".into()).into())
+        .ok_or_else(|| OSError::NotFound("Album".into()))?;
+    // Distinct and order by does not work together. We can not eliminate distinct because of
+    // genres.
+    Ok((album, song_ids.into_iter().unique().collect()))
 }
 
 pub async fn get_album(
@@ -244,5 +251,46 @@ mod tests {
                 .unwrap(),
             OSError::NotFound(_)
         ));
+    }
+
+    #[tokio::test]
+    async fn test_get_album_id3_sorted() {
+        let album_name = "album";
+        let n_song = 5_usize;
+        let mut infra = Infra::new().await.n_folder(1).await.add_user(None).await;
+        infra
+            .add_songs(
+                0,
+                (0..n_song)
+                    .map(|i| SongTag {
+                        album: album_name.into(),
+                        track_number: Some((i + 1) as _),
+                        disc_number: Some(1),
+                        ..Faker.fake()
+                    })
+                    .collect(),
+            )
+            .await
+            .add_songs(
+                0,
+                (0..n_song)
+                    .map(|i| SongTag {
+                        album: album_name.into(),
+                        track_number: Some((i + 1) as _),
+                        disc_number: Some(2),
+                        ..Faker.fake()
+                    })
+                    .collect(),
+            )
+            .await
+            .scan(.., None)
+            .await;
+
+        let album_id = upsert_album(infra.pool(), album_name.into()).await.unwrap();
+        let songs = get_album(infra.pool(), infra.user_id(0), album_id).await.unwrap().songs;
+        for (i, song) in songs.into_iter().enumerate() {
+            assert_eq!(song.track.unwrap() as usize, (i % 5) + 1);
+            assert_eq!(song.disc_number.unwrap() as usize, (i / 5) + 1);
+        }
     }
 }
