798ac97e4a404d3fe32eb8ae2ad560a2020dd0a6
Vo Van Nghia <git@vnghia.com>
2024-05-13 23:22:47 +0200

    chore(rust): bump rust toolchain to 2024-05-12

--- lastfm-client/build.rs
+++ lastfm-client/build.rs
@@ -1,5 +1,6 @@
 fn main() {
+    println!("cargo::rustc-check-cfg=cfg(lastfm_env)");
     if std::env::var("LASTFM_KEY").is_ok_and(|s| !s.is_empty()) {
-        println!("cargo::rustc-cfg=lastfm_env")
+        println!("cargo::rustc-cfg=lastfm_env");
     }
 }
