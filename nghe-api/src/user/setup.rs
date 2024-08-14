e11ed73f726dbe5d000b22dd585244488b92bfa9
Vo Van Nghia <git@vnghia.com>
2024-08-14 11:31:13 +0200

    refactor: add api/backend/proc-macro crate

---
+++ nghe-api/src/user/setup.rs
@@ -0,0 +1,12 @@
+use nghe_proc_macro::Endpoint;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Endpoint)]
+pub struct Request {
+    pub username: String,
+    pub password: String,
+    pub email: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Response {}
