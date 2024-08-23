1abc76cea59aeb117e12e1931ecdbefc1d41e344
Vo Van Nghia <git@vnghia.com>
2024-08-23 09:41:55 +0200

    add minimal main for the new backend

---
+++ nghe-backend/src/main.rs
@@ -0,0 +1,11 @@
+use axum::Router;
+
+#[tokio::main]
+async fn main() {
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app()).await.unwrap();
+}
+
+fn app() -> Router {
+    Router::new()
+}
