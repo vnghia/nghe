eb58be1c14969729f761e7439d90f51fc25c6dae
Vo Van Nghia <git@vnghia.com>
2024-08-24 18:40:28 +0200

    implement setup handler

--- nghe-backend/src/app/user/setup.rs
+++ nghe-backend/src/app/user/setup.rs
@@ -1,12 +1,35 @@
 #![allow(clippy::unused_async)]
 
+use diesel::QueryDsl;
+use diesel_async::RunQueryDsl;
 pub use nghe_api::user::setup::{Request, Response};
 use nghe_proc_macro::handler;
 
 use crate::app::error::Error;
 use crate::app::state::Database;
+use crate::app::user::create;
+use crate::orm::users;
 
 #[handler]
 pub async fn handler(database: &Database, request: Request) -> Result<Response, Error> {
-    Ok(Response {})
+    if users::dsl::table.count().first::<i64>(&mut database.get().await?).await? > 0 {
+        Err(Error::Unauthorized("Could not access setup endpoint when there is already one user"))
+    } else {
+        let Request { username, password, email } = request;
+        create::handler(
+            database,
+            create::Request {
+                username,
+                password,
+                email,
+                admin: true,
+                stream: true,
+                download: true,
+                share: true,
+                allow: false,
+            },
+        )
+        .await?;
+        Ok(Response {})
+    }
 }
