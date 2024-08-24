5437a16f322b620f999ee6d9bca988ee32ec9621
Vo Van Nghia <git@vnghia.com>
2024-08-24 18:25:17 +0200

    add create user

---
+++ nghe-backend/src/app/user/create.rs
@@ -0,0 +1,29 @@
+use diesel_async::RunQueryDsl;
+pub use nghe_api::user::create::{Request, Response};
+use nghe_proc_macro::handler;
+
+use crate::app::error::Error;
+use crate::app::state::Database;
+use crate::orm::users;
+
+#[handler]
+pub async fn handler(database: &Database, request: Request) -> Result<Response, Error> {
+    let Request { username, password, email, admin, stream, download, share, allow } = request;
+    let password = database.encrypt(password);
+
+    let user_id = diesel::insert_into(users::dsl::table)
+        .values(users::New {
+            password: password.into(),
+            data: users::Data {
+                username: username.into(),
+                email: email.into(),
+                role: users::Role { admin, stream, download, share },
+            },
+        })
+        .returning(users::dsl::id)
+        .get_result(&mut database.get().await?)
+        .await
+        .map_err(color_eyre::Report::new)?;
+
+    Ok(Response { user_id })
+}
