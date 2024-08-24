b759796fca283b84bd23b277fa2ae3590c92013a
Vo Van Nghia <git@vnghia.com>
2024-08-24 21:30:07 +0200

    add minimal testing mock

---
+++ nghe-backend/src/test/database/mod.rs
@@ -0,0 +1,67 @@
+use concat_string::concat_string;
+use diesel_async::{AsyncConnection, AsyncPgConnection};
+use url::Url;
+use uuid::Uuid;
+
+use crate::app::state;
+use crate::{config, migration};
+
+pub struct Mock {
+    name: String,
+    url: String,
+
+    pub state: state::Database,
+}
+
+impl Mock {
+    pub async fn new() -> Self {
+        let url = std::env::var("DATABASE_URL").unwrap();
+
+        let _ = tracing_subscriber::fmt()
+            .with_env_filter(
+                tracing_subscriber::EnvFilter::try_from_default_env()
+                    .unwrap_or_else(|_| "info".into()),
+            )
+            .with_test_writer()
+            .try_init();
+
+        let name = Uuid::new_v4().to_string();
+        let mut mock_url = Url::parse(&url).unwrap();
+        mock_url.set_path(&name);
+
+        let mut root_conn = AsyncPgConnection::establish(&url).await.unwrap();
+        diesel_async::RunQueryDsl::execute(
+            diesel::sql_query(concat_string!("CREATE DATABASE \"", name, "\";")),
+            &mut root_conn,
+        )
+        .await
+        .unwrap();
+
+        let mock_url = mock_url.to_string();
+        migration::run(&mock_url).await;
+
+        Self {
+            name,
+            url,
+            state: state::Database::new(&config::Database { url: mock_url, key: rand::random() }),
+        }
+    }
+}
+
+#[cfg(not(any(target_env = "musl", all(target_arch = "aarch64", target_os = "linux"))))]
+impl Drop for Mock {
+    fn drop(&mut self) {
+        use diesel::pg::PgConnection;
+        use diesel::Connection;
+
+        let raw_statement =
+            concat_string!("DROP DATABASE IF EXISTS \"", &self.name, "\" WITH (FORCE);");
+        if let Err::<_, color_eyre::Report>(e) = try {
+            let mut conn = PgConnection::establish(&self.url)?;
+            diesel::RunQueryDsl::execute(diesel::sql_query(&raw_statement), &mut conn)?;
+        } {
+            println!("Could not drop temporary database because of {:?}", &e);
+            println!("Please drop the database manually with '{}'", &raw_statement);
+        }
+    }
+}
