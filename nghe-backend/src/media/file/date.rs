6262ce6f05c37f1bf613c30b1a7dbeedddd0ecdb
Vo Van Nghia <git@vnghia.com>
2024-09-03 05:24:06 +0200

    add media date

---
+++ nghe-backend/src/media/file/date.rs
@@ -0,0 +1,90 @@
+use std::num::NonZeroU8;
+use std::str::FromStr;
+
+use time::macros::format_description;
+use time::Month;
+
+use crate::Error;
+
+type FormatDescription<'a> = &'a [time::format_description::BorrowedFormatItem<'a>];
+
+const YMD_MINUS_FORMAT: FormatDescription = format_description!("[year]-[month]-[day]");
+const YM_MINUS_FORMAT: FormatDescription = format_description!("[year]-[month]");
+const YMD_SLASH_FORMAT: FormatDescription = format_description!("[year]/[month]/[day]");
+const YM_SLASH_FORMAT: FormatDescription = format_description!("[year]/[month]");
+const YMD_DOT_FORMAT: FormatDescription = format_description!("[year].[month].[day]");
+const YM_DOT_FORMAT: FormatDescription = format_description!("[year].[month]");
+const Y_FORMAT: FormatDescription = format_description!("[year]");
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct Date {
+    pub year: Option<i32>,
+    pub month: Option<Month>,
+    pub day: Option<NonZeroU8>,
+}
+
+impl FromStr for Date {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut parsed = time::parsing::Parsed::new();
+        let input = s.as_bytes();
+        if !s.is_empty()
+            && parsed.parse_items(input, YMD_MINUS_FORMAT).is_err()
+            && parsed.parse_items(input, YMD_SLASH_FORMAT).is_err()
+            && parsed.parse_items(input, YMD_DOT_FORMAT).is_err()
+            && parsed.parse_items(input, YM_MINUS_FORMAT).is_err()
+            && parsed.parse_items(input, YM_SLASH_FORMAT).is_err()
+            && parsed.parse_items(input, YM_DOT_FORMAT).is_err()
+        {
+            // Don't aggresively parse everything as year
+            let result = parsed.parse_items(input, Y_FORMAT);
+            if result.is_err() || result.is_ok_and(|remain| !remain.is_empty()) {
+                return Err(Error::MediaDateFormat(s.to_owned()));
+            }
+        }
+
+        Ok(Self { year: parsed.year(), month: parsed.month(), day: parsed.day() })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+
+    use super::*;
+
+    #[rstest]
+    #[case("2000-12-01", Some(2000), Some(Month::December), Some(1))]
+    #[case("2000/12/01", Some(2000), Some(Month::December), Some(1))]
+    #[case("2000.12.01", Some(2000), Some(Month::December), Some(1))]
+    #[case("2000-12-01-still-ok", Some(2000), Some(Month::December), Some(1))]
+    #[case("2000.12.01/still/ok", Some(2000), Some(Month::December), Some(1))]
+    #[case("2000-12-01T02:29:01Z", Some(2000), Some(Month::December), Some(1))]
+    #[case("2000-12", Some(2000), Some(Month::December), None)]
+    #[case("2000/12", Some(2000), Some(Month::December), None)]
+    #[case("2000.12", Some(2000), Some(Month::December), None)]
+    #[case("2000", Some(2000), None, None)]
+    #[case("", None, None, None)]
+    fn test_parse_success(
+        #[case] input: &'static str,
+        #[case] year: Option<i32>,
+        #[case] month: Option<Month>,
+        #[case] day: Option<u8>,
+    ) {
+        let date: Date = input.parse().unwrap();
+        assert_eq!(date.year, year);
+        assert_eq!(date.month, month);
+        assert_eq!(date.day.map(NonZeroU8::get), day);
+    }
+
+    #[rstest]
+    #[case("2000-31")]
+    #[case("20-12-01")]
+    #[case("invalid")]
+    #[case("12-01")]
+    #[case("31")]
+    fn test_parse_error(#[case] input: &'static str) {
+        assert!(input.parse::<Date>().is_err());
+    }
+}
