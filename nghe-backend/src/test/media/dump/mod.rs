ec1f17de5210527f1ae354151f145ac2a2c57d5c
Vo Van Nghia <git@vnghia.com>
2024-09-06 20:06:54 +0200

    remove enum dispatch

--- nghe-backend/src/test/media/dump/mod.rs
+++ nghe-backend/src/test/media/dump/mod.rs
@@ -3,13 +3,11 @@ mod tag;
 
 use std::borrow::Cow;
 
-use enum_dispatch::enum_dispatch;
 use isolang::Language;
 
 use crate::config;
 use crate::media::file::{Artists, Common, File, Metadata, TrackDisc};
 
-#[enum_dispatch(File)]
 pub trait MetadataDumper {
     fn dump_song(&mut self, config: &config::Parsing, song: Common<'_>);
     fn dump_album(&mut self, config: &config::Parsing, album: Common<'_>);
@@ -29,5 +27,49 @@ pub trait MetadataDumper {
         self.dump_languages(config, languages);
         self.dump_genres(config, genres);
         self.dump_compilation(config, compilation);
+    }
+}
+
+impl MetadataDumper for File {
+    fn dump_song(&mut self, config: &config::Parsing, song: Common<'_>) {
+        match self {
+            File::Flac(file) => file.dump_song(config, song),
+        }
+    }
+
+    fn dump_album(&mut self, config: &config::Parsing, album: Common<'_>) {
+        match self {
+            File::Flac(file) => file.dump_album(config, album),
+        }
+    }
+
+    fn dump_artists(&mut self, config: &config::Parsing, artists: Artists<'_>) {
+        match self {
+            File::Flac(file) => file.dump_artists(config, artists),
+        }
+    }
+
+    fn dump_track_disc(&mut self, config: &config::Parsing, track_disc: TrackDisc) {
+        match self {
+            File::Flac(file) => file.dump_track_disc(config, track_disc),
+        }
+    }
+
+    fn dump_languages(&mut self, config: &config::Parsing, languages: Vec<Language>) {
+        match self {
+            File::Flac(file) => file.dump_languages(config, languages),
+        }
+    }
+
+    fn dump_genres(&mut self, config: &config::Parsing, genres: Vec<Cow<'_, str>>) {
+        match self {
+            File::Flac(file) => file.dump_genres(config, genres),
+        }
+    }
+
+    fn dump_compilation(&mut self, config: &config::Parsing, compilation: bool) {
+        match self {
+            File::Flac(file) => file.dump_compilation(config, compilation),
+        }
     }
 }
