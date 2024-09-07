d89ba81efa748e8933415ca66c68d8455cb0bd8d
Vo Van Nghia <git@vnghia.com>
2024-09-07 11:17:24 +0200

    add test for check folder

--- nghe-backend/src/filesystem/common.rs
+++ nghe-backend/src/filesystem/common.rs
@@ -49,3 +49,31 @@ impl<'fs> Trait for Impl<'fs> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use nghe_api::common::filesystem;
+    use rstest::rstest;
+
+    use super::*;
+    use crate::test::{mock, Mock};
+
+    #[rstest]
+    #[case(filesystem::Type::Local, "tmp", false)]
+    #[cfg_attr(unix, case(filesystem::Type::Local, "/tmp/", true))]
+    #[cfg_attr(unix, case(filesystem::Type::Local, "C:\\Windows", false))]
+    #[cfg_attr(windows, case(filesystem::Type::Local, "C:\\Windows", true))]
+    #[cfg_attr(windows, case(filesystem::Type::Local, "/tmp/", false))]
+    #[tokio::test]
+    async fn test_check_folder(
+        #[future(awt)]
+        #[with(0, 0)]
+        mock: Mock,
+        #[case] filesystem_type: filesystem::Type,
+        #[case] path: &str,
+        #[case] is_ok: bool,
+    ) {
+        let filesystem = mock.to_impl(filesystem_type);
+        assert_eq!(filesystem.check_folder(path.into()).await.is_ok(), is_ok);
+    }
+}
