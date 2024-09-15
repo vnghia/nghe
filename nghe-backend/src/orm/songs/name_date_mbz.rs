a7afb3eabb41ec934faefdef550e22afc4a149c5
Vo Van Nghia <git@vnghia.com>
2024-09-16 00:05:20 +0200

    add song orm name media ibz

---
+++ nghe-backend/src/orm/songs/name_date_mbz.rs
@@ -0,0 +1,22 @@
+use std::borrow::Cow;
+
+use diesel::prelude::*;
+use diesel_derives::AsChangeset;
+use uuid::Uuid;
+
+use super::{date, songs};
+
+#[derive(Debug, Queryable, Selectable, Insertable, AsChangeset)]
+#[diesel(table_name = songs, check_for_backend(crate::orm::Type))]
+#[diesel(treat_none_as_null = true)]
+pub struct NameDateMbz<'a> {
+    #[diesel(column_name = title)]
+    pub name: Cow<'a, str>,
+    #[diesel(embed)]
+    pub date: date::Date,
+    #[diesel(embed)]
+    pub release_date: date::Release,
+    #[diesel(embed)]
+    pub original_release_date: date::OriginalRelease,
+    pub mbz_id: Option<Uuid>,
+}
