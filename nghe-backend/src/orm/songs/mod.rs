253d12eac2ad828a737bf2029dcf050f34c60498
Vo Van Nghia <git@vnghia.com>
2024-09-16 00:48:04 +0200

    add song orm data

--- nghe-backend/src/orm/songs/mod.rs
+++ nghe-backend/src/orm/songs/mod.rs
@@ -1,6 +1,11 @@
+use diesel::prelude::*;
+use diesel_derives::AsChangeset;
+
 pub mod date;
 pub mod name_date_mbz;
 pub mod position;
+
+use std::borrow::Cow;
 
 use crate::schema::songs;
 
@@ -9,3 +14,14 @@ pub mod schema {
 }
 
 pub use schema::table;
+
+#[derive(Debug, Queryable, Selectable, Insertable, AsChangeset)]
+#[diesel(table_name = songs, check_for_backend(crate::orm::Type))]
+#[diesel(treat_none_as_null = true)]
+pub struct Data<'a> {
+    #[diesel(embed)]
+    pub main: name_date_mbz::NameDateMbz<'a>,
+    #[diesel(embed)]
+    pub track_disc: position::TrackDisc,
+    pub languages: Vec<Cow<'a, str>>,
+}
