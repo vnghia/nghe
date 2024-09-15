b488f79a2b90256c4037d31e9fa9db1d042f1339
Vo Van Nghia <git@vnghia.com>
2024-09-16 00:48:04 +0200

    add song orm data

--- nghe-backend/src/media/file/metadata.rs
+++ nghe-backend/src/media/file/metadata.rs
@@ -1,18 +1,27 @@
 use std::borrow::Cow;
+use std::str::FromStr;
 
 #[cfg(test)]
 use fake::{Dummy, Fake};
 use isolang::Language;
 #[cfg(test)]
 use itertools::Itertools;
+use o2o::o2o;
 
 use super::{artist, name_date_mbz, position};
+use crate::orm::songs;
+use crate::Error;
 
-#[derive(Debug)]
+#[derive(Debug, o2o)]
+#[try_map_owned(songs::Data<'a>, Error)]
 #[cfg_attr(test, derive(PartialEq, Eq, Dummy, Clone))]
 pub struct Song<'a> {
+    #[map(~.try_into()?)]
     pub main: name_date_mbz::NameDateMbz<'a>,
+    #[map(~.try_into()?)]
     pub track_disc: position::TrackDisc,
+    #[from(~.into_iter().map(|language| Language::from_str(language.as_ref())).try_collect()?)]
+    #[into(~.into_iter().map(|language| language.to_639_3().into()).collect())]
     #[cfg_attr(
         test,
         dummy(expr = "((0..=7915), \
