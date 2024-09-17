6b8af0591764277897a9a27e5200d69af9498ee4
Vo Van Nghia <git@vnghia.com>
2024-09-17 13:49:28 +0200

    rename audio type to audio format

--- nghe-backend/src/test/assets/mod.rs
+++ nghe-backend/src/test/assets/mod.rs
@@ -7,6 +7,6 @@ pub fn dir() -> Utf8TypedPathBuf {
     path::Local::from_str(&env!("CARGO_MANIFEST_DIR")).parent().unwrap().join("assets").join("test")
 }
 
-pub fn path(ty: audio::Type) -> Utf8TypedPathBuf {
-    dir().join("sample").with_extension(ty.as_ref())
+pub fn path(format: audio::Format) -> Utf8TypedPathBuf {
+    dir().join("sample").with_extension(format.as_ref())
 }
