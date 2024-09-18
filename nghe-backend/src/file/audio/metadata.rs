bd98d813927d6475b9736303ed24407cbb74c242
Vo Van Nghia <git@vnghia.com>
2024-09-18 06:38:45 +0200

    add test song update roundtrip

--- nghe-backend/src/file/audio/metadata.rs
+++ nghe-backend/src/file/audio/metadata.rs
@@ -14,9 +14,11 @@ use crate::Error;
 
 #[derive(Debug, o2o)]
 #[try_map_owned(songs::Song<'a>, Error)]
+#[ref_try_into(songs::Song<'a>, Error)]
 #[cfg_attr(test, derive(PartialEq, Eq, Dummy, Clone))]
 pub struct Song<'a> {
-    #[map(~.try_into()?)]
+    #[map_owned(~.try_into()?)]
+    #[ref_into((&~).try_into()?)]
     pub main: name_date_mbz::NameDateMbz<'a>,
     #[map(~.try_into()?)]
     pub track_disc: position::TrackDisc,
@@ -25,7 +27,7 @@ pub struct Song<'a> {
             || Error::LanguageFromDatabaseIsNull)?.as_ref()
         ).map_err(Error::from)
     ).try_collect()?)]
-    #[into(~.into_iter().map(|language| Some(language.to_639_3().into())).collect())]
+    #[into(~.iter().map(|language| Some(language.to_639_3().into())).collect())]
     #[cfg_attr(
         test,
         dummy(expr = "((0..=7915), \
@@ -57,6 +59,8 @@ mod test {
     use uuid::Uuid;
 
     use super::*;
+    use crate::file;
+    use crate::file::audio::{self, Audio};
     use crate::orm::albums;
     use crate::test::{mock, Mock};
 
@@ -68,7 +72,7 @@ mod test {
         let album: name_date_mbz::NameDateMbz = Faker.fake();
         let upsert = albums::Upsert {
             music_folder_id: mock.music_folder(0).await.music_folder.id,
-            data: album.clone().try_into().unwrap(),
+            data: (&album).try_into().unwrap(),
         };
         let id = if let Some(id) = id {
             diesel::update(albums::table)
@@ -89,6 +93,34 @@ mod test {
         (id, album)
     }
 
+    async fn upsert_song(mock: &Mock, id: impl Into<Option<Uuid>>) -> (Uuid, Audio) {
+        let id = id.into();
+        let audio: Audio = Faker.fake();
+        let (album_id, _) = upsert_album(mock, None).await;
+        let upsert = songs::Upsert {
+            album_id,
+            relative_path: Faker.fake::<String>().into(),
+            data: (&audio).try_into().unwrap(),
+        };
+        let id = if let Some(id) = id {
+            diesel::update(songs::table)
+                .filter(songs::id.eq(id))
+                .set(upsert)
+                .execute(&mut mock.get().await)
+                .await
+                .unwrap();
+            id
+        } else {
+            diesel::insert_into(songs::table)
+                .values(upsert)
+                .returning(songs::id)
+                .get_result(&mut mock.get().await)
+                .await
+                .unwrap()
+        };
+        (id, audio)
+    }
+
     async fn select_album(mock: &Mock, id: Uuid) -> name_date_mbz::NameDateMbz {
         albums::table
             .filter(albums::id.eq(id))
@@ -97,6 +129,15 @@ mod test {
             .await
             .unwrap()
             .try_into()
+            .unwrap()
+    }
+
+    async fn select_song(mock: &Mock, id: Uuid) -> songs::Data {
+        songs::table
+            .filter(songs::id.eq(id))
+            .select(songs::Data::as_select())
+            .get_result(&mut mock.get().await)
+            .await
             .unwrap()
     }
 
@@ -115,5 +156,32 @@ mod test {
         let (id, album) = upsert_album(&mock, id).await;
         let database_album = select_album(&mock, id).await;
         assert_eq!(database_album, album);
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn test_song_roundtrip(#[future(awt)] mock: Mock) {
+        let (id, audio) = upsert_song(&mock, None).await;
+        let database_data = select_song(&mock, id).await;
+        let database_song: Song = database_data.song.try_into().unwrap();
+        let database_property: audio::Property = database_data.property.try_into().unwrap();
+        let database_file: file::property::File<_> = database_data.file.into();
+        assert_eq!(database_song, audio.metadata.song);
+        assert_eq!(database_property, audio.property);
+        assert_eq!(database_file, audio.file);
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn test_song_update_roundtrip(#[future(awt)] mock: Mock) {
+        let (id, _) = upsert_song(&mock, None).await;
+        let (id, audio) = upsert_song(&mock, id).await;
+        let database_data = select_song(&mock, id).await;
+        let database_song: Song = database_data.song.try_into().unwrap();
+        let database_property: audio::Property = database_data.property.try_into().unwrap();
+        let database_file: file::property::File<_> = database_data.file.into();
+        assert_eq!(database_song, audio.metadata.song);
+        assert_eq!(database_property, audio.property);
+        assert_eq!(database_file, audio.file);
     }
 }
--- nghe-backend/src/file/audio/name_date_mbz.rs
+++ nghe-backend/src/file/audio/name_date_mbz.rs
@@ -12,8 +12,11 @@ use crate::Error;
 #[derive(Debug, o2o)]
 #[try_map_owned(songs::name_date_mbz::NameDateMbz<'a>, Error)]
 #[try_map_owned(albums::Data<'a>, Error)]
+#[ref_try_into(songs::name_date_mbz::NameDateMbz<'a>, Error)]
+#[ref_try_into(albums::Data<'a>, Error)]
 #[cfg_attr(test, derive(PartialEq, Eq, Dummy, Clone))]
 pub struct NameDateMbz<'a> {
+    #[ref_into(Cow::Borrowed(~.as_ref()))]
     #[cfg_attr(test, dummy(expr = "Faker.fake::<String>().into()"))]
     pub name: Cow<'a, str>,
     #[map(~.try_into()?)]
