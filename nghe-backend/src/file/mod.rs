b36beeb61e9cb83b670a2148bf6b86163da0d982
Vo Van Nghia <git@vnghia.com>
2024-09-19 20:30:23 +0200

    rename property::File to file::Property

--- nghe-backend/src/file/mod.rs
+++ nghe-backend/src/file/mod.rs
@@ -1,20 +1,27 @@
 pub mod audio;
-pub mod property;
 
 use xxhash_rust::xxh3::xxh3_64;
 
 use crate::Error;
 
+#[derive(Debug, Clone, Copy)]
+#[cfg_attr(test, derive(PartialEq, Eq, fake::Dummy))]
+pub struct Property<F> {
+    pub hash: u64,
+    pub size: u32,
+    pub format: F,
+}
+
 #[derive(Debug)]
 pub struct File<F> {
     pub data: Vec<u8>,
-    pub property: property::File<F>,
+    pub property: Property<F>,
 }
 
 impl<F> File<F> {
     pub fn new(data: Vec<u8>, format: F) -> Result<Self, Error> {
         let hash = xxh3_64(&data);
         let size = data.len().try_into()?;
-        Ok(Self { data, property: property::File { hash, size, format } })
+        Ok(Self { data, property: Property { hash, size, format } })
     }
 }
