4152a921b76ab01f1708c1ed09078abe214289c7
Vo Van Nghia <git@vnghia.com>
2024-09-26 15:08:56 +0200

    add compute artist index

--- nghe-backend/src/file/audio/artist.rs
+++ nghe-backend/src/file/audio/artist.rs
@@ -3,6 +3,7 @@ use std::borrow::Cow;
 #[cfg(test)]
 use fake::{Dummy, Fake, Faker};
 use o2o::o2o;
+use unicode_normalization::UnicodeNormalization;
 use uuid::Uuid;
 
 use crate::orm::artists;
@@ -25,6 +26,33 @@ pub struct Artists<'a> {
     pub song: Vec<Artist<'a>>,
     pub album: Vec<Artist<'a>>,
     pub compilation: bool,
+}
+
+impl<'a> Artist<'a> {
+    pub fn index(&self, prefixes: &[impl AsRef<str>]) -> Result<char, Error> {
+        let mut iter = prefixes.iter();
+        let name = loop {
+            match iter.next() {
+                Some(prefix) => {
+                    if let Some(name) = self.name.strip_prefix(prefix.as_ref()) {
+                        break name;
+                    }
+                }
+                None => break self.name.as_ref(),
+            }
+        };
+        name.nfkd().next().ok_or_else(|| Error::MediaArtistNameEmpty).map(|c| {
+            if c.is_ascii_alphabetic() {
+                c.to_ascii_uppercase()
+            } else if c.is_numeric() {
+                '#'
+            } else if !c.is_alphabetic() {
+                '*'
+            } else {
+                c
+            }
+        })
+    }
 }
 
 impl<'a> Artists<'a> {
@@ -63,5 +91,25 @@ mod test {
         fn from(value: (&'a str, Uuid)) -> Self {
             Self { name: value.0.into(), mbz_id: Some(value.1) }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+
+    use super::*;
+
+    #[rstest]
+    #[case("The One", &["The ", "A "], 'O')]
+    #[case("The 1", &["The ", "A "], '#')]
+    #[case("The one", &["The ", "A "], 'O')]
+    #[case("狼", &["The ", "A "], '狼')]
+    #[case("é", &["The ", "A "], 'E')]
+    #[case("ド", &["The ", "A "], 'ト')]
+    #[case("ａ", &["The ", "A "], 'A')]
+    #[case("%", &["The ", "A "], '*')]
+    fn test_index(#[case] name: &str, #[case] prefixes: &[&str], #[case] index: char) {
+        assert_eq!(Artist { ..name.into() }.index(prefixes).unwrap(), index);
     }
 }
