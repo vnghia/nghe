e817435a6040a848510bb33946bd1c4b2d9eff9d
Vo Van Nghia <git@vnghia.com>
2024-09-26 09:57:01 +0200

    add songs_genres

--- nghe-backend/src/orm/genres.rs
+++ nghe-backend/src/orm/genres.rs
@@ -8,6 +8,6 @@ pub use crate::schema::genres::{self, *};
 #[derive(Debug, Queryable, Selectable, Insertable, AsChangeset)]
 #[diesel(table_name = genres, check_for_backend(crate::orm::Type))]
 #[diesel(treat_none_as_null = true)]
-pub struct Upsert<'a> {
+pub struct Data<'a> {
     pub value: Cow<'a, str>,
 }
