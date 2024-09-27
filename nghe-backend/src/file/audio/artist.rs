d6eb02df1c646c791197d20399b7b27943aed0d2
Vo Van Nghia <git@vnghia.com>
2024-09-27 13:28:44 +0200

    move all tests out of orm

--- nghe-backend/src/file/audio/artist.rs
+++ nghe-backend/src/file/audio/artist.rs
@@ -285,6 +285,8 @@ mod tests {
     use rstest::rstest;
 
     use super::*;
+    use crate::file::audio;
+    use crate::test::{mock, Mock};
 
     #[rstest]
     #[case("The One", &["The ", "A "], 'O')]
@@ -328,5 +330,64 @@ mod tests {
             .compilation(),
             result
         );
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn test_artist_upsert_roundtrip(
+        #[future(awt)] mock: Mock,
+        #[values(true, false)] mbz_id: bool,
+        #[values(true, false)] update_artist: bool,
+    ) {
+        let mbz_id = if mbz_id { Some(Faker.fake()) } else { None };
+        let artist = Artist { mbz_id, ..Faker.fake() };
+        let id = artist.upsert(mock.database(), &[""]).await.unwrap();
+        let database_artist = Artist::query(&mock, id).await;
+        assert_eq!(database_artist, artist);
+
+        if update_artist {
+            let update_artist = Artist { mbz_id, ..Faker.fake() };
+            let update_id = update_artist.upsert(mock.database(), &[""]).await.unwrap();
+            let database_update_artist = Artist::query(&mock, id).await;
+            if mbz_id.is_some() {
+                assert_eq!(id, update_id);
+                assert_eq!(database_update_artist, update_artist);
+            } else {
+                // This will always insert a new row to the database
+                // since there is nothing to identify an old artist.
+                assert_ne!(id, update_id);
+            }
+        }
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn test_artist_upsert_no_mbz_id(#[future(awt)] mock: Mock) {
+        // We want to make sure that insert the same artist with no mbz_id
+        // twice does not result in any error.
+        let artist = Artist { mbz_id: None, ..Faker.fake() };
+        let id = artist.upsert(mock.database(), &[""]).await.unwrap();
+        let update_id = artist.upsert(mock.database(), &[""]).await.unwrap();
+        assert_eq!(update_id, id);
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn test_artists_upsert(
+        #[future(awt)] mock: Mock,
+        #[values(true, false)] compilation: bool,
+    ) {
+        let information: audio::Information = Faker.fake();
+        let album_id = information.metadata.album.upsert_mock(&mock, 0).await;
+        let song_id = information
+            .upsert_song(mock.database(), album_id, Faker.fake::<String>(), None)
+            .await
+            .unwrap();
+
+        let artists = Artists { compilation, ..Faker.fake() };
+        artists.upsert(mock.database(), &[""], song_id).await.unwrap();
+
+        let database_artists = Artists::query(&mock, song_id).await;
+        assert_eq!(database_artists, artists);
     }
 }
