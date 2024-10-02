baa25b72ee72ea7c4d2020d9678c9d3d3e689c5f
Vo Van Nghia <git@vnghia.com>
2024-10-02 11:13:03 +0200

    add upsert mock information

--- nghe-backend/src/file/audio/name_date_mbz.rs
+++ nghe-backend/src/file/audio/name_date_mbz.rs
@@ -48,9 +48,7 @@ mod test {
 
     impl<'a> Album<'a> {
         pub async fn upsert_mock(&self, mock: &Mock, index: usize) -> Uuid {
-            self.upsert(mock.database(), mock.music_folder(index).await.music_folder.id)
-                .await
-                .unwrap()
+            self.upsert(mock.database(), mock.music_folder(index).await.id()).await.unwrap()
         }
     }
 
