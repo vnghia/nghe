4f6e9460b08a55063eb24c3aba0202b5bb4bbda2
Vo Van Nghia <git@vnghia.com>
2024-11-07 11:41:03 +0100

    add property path

--- nghe-backend/src/file/mod.rs
+++ nghe-backend/src/file/mod.rs
@@ -1,6 +1,7 @@
 pub mod audio;
 
 use nghe_api::common::format;
+use typed_path::{Utf8NativePath, Utf8NativePathBuf};
 use xxhash_rust::xxh3::xxh3_64;
 
 use crate::Error;
@@ -28,6 +29,25 @@ impl<F: format::Format> Property<F> {
 
     pub fn replace<FN: format::Format>(&self, format: FN) -> Property<FN> {
         Property { hash: self.hash, size: self.size, format }
+    }
+
+    pub fn path(
+        &self,
+        base: impl AsRef<Utf8NativePath>,
+        name: impl Into<Option<&'static str>>,
+    ) -> Utf8NativePathBuf {
+        let hash = self.hash.to_le_bytes();
+
+        // Avoid putting too many files in a single directory
+        let first = faster_hex::hex_string(&hash[..1]);
+        let second = faster_hex::hex_string(&hash[1..]);
+
+        let path = base.as_ref().join(first).join(second).join(self.size.to_string());
+        if let Some(name) = name.into() {
+            path.join(name).with_extension(self.format.extension())
+        } else {
+            path
+        }
     }
 }
 
