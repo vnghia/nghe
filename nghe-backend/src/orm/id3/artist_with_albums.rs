04827c2c8e65479aafec4cc85f284be54a568f1f
Vo Van Nghia <git@vnghia.com>
2024-11-19 14:31:41 +0100

    backend/browsing: add test for query artist with albums

--- nghe-backend/src/orm/id3/artist_with_albums.rs
+++ nghe-backend/src/orm/id3/artist_with_albums.rs
@@ -53,3 +53,87 @@ pub mod query {
         artist.select(artist_with_albums)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use diesel_async::RunQueryDsl;
+    use fake::{Fake, Faker};
+    use rstest::rstest;
+
+    use super::*;
+    use crate::file::audio;
+    use crate::schema::artists;
+    use crate::test::{mock, Mock};
+
+    #[rstest]
+    #[tokio::test]
+    async fn test_query_artist(#[future(awt)] mock: Mock, #[values(0, 6)] n_album: i64) {
+        let artist: audio::Artist = Faker.fake();
+        let artist_id = artist.upsert_mock(&mock).await;
+
+        mock.add_audio_artist(0, [artist.clone()], [Faker.fake()], false, 1).await;
+        mock.add_audio_artist(
+            0,
+            [Faker.fake()],
+            [artist.clone()],
+            false,
+            n_album.try_into().unwrap(),
+        )
+        .await;
+
+        let database_artist = query::with_user_id(mock.user_id(0).await)
+            .filter(artists::id.eq(artist_id))
+            .get_result(&mut mock.get().await)
+            .await
+            .unwrap();
+
+        assert_eq!(database_artist.artist.album_count, n_album);
+        let n_album: usize = n_album.try_into().unwrap();
+        assert_eq!(database_artist.albums.len(), n_album);
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn test_query_partial(
+        #[future(awt)]
+        #[with(1, 0)]
+        mock: Mock,
+        #[values(true, false)] allow: bool,
+    ) {
+        mock.add_music_folder().allow(allow).call().await;
+        mock.add_music_folder().call().await;
+
+        let artist: audio::Artist = Faker.fake();
+        let artist_id = artist.upsert_mock(&mock).await;
+
+        let n_album = (2..4).fake();
+
+        let album: audio::Album = Faker.fake();
+        let album_id = album.upsert_mock(&mock, 0).await;
+        mock.music_folder(0)
+            .await
+            .add_audio()
+            .album(album)
+            .artists(audio::Artists {
+                album: [artist.clone()].into(),
+                compilation: false,
+                ..Faker.fake()
+            })
+            .call()
+            .await;
+
+        mock.add_audio_artist(1, [Faker.fake()], [artist.clone()], false, n_album).await;
+
+        let database_artist = query::with_user_id(mock.user_id(0).await)
+            .filter(artists::id.eq(artist_id))
+            .get_result(&mut mock.get().await)
+            .await
+            .unwrap();
+
+        let n_album = if allow { n_album + 1 } else { n_album };
+        assert_eq!(database_artist.albums.len(), n_album);
+        let n_album: i64 = n_album.try_into().unwrap();
+        assert_eq!(database_artist.artist.album_count, n_album);
+        assert_eq!(database_artist.albums.contains(&album_id), allow);
+    }
+}
