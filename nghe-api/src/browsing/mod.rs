5b12577c69b5ea693cd339e325f26d6e84992c05
Vo Van Nghia <git@vnghia.com>
2024-11-21 11:32:12 +0100

    api/browsing: add api/handler for get_album_info2

--- nghe-api/src/browsing/mod.rs
+++ nghe-api/src/browsing/mod.rs
@@ -1,4 +1,5 @@
 pub mod get_album;
+pub mod get_album_info2;
 pub mod get_artist;
 pub mod get_artists;
 pub mod get_genres;
--- nghe-backend/src/route/browsing/mod.rs
+++ nghe-backend/src/route/browsing/mod.rs
@@ -1,4 +1,5 @@
 mod get_album;
+mod get_album_info2;
 mod get_artist;
 mod get_artists;
 mod get_genres;
@@ -6,5 +7,7 @@ mod get_music_folders;
 mod get_song;
 
 nghe_proc_macro::build_router! {
-    modules = [get_album, get_artist, get_artists, get_genres, get_music_folders, get_song],
+    modules = [
+        get_album, get_album_info2, get_artist, get_artists, get_genres, get_music_folders, get_song
+    ],
 }
