4ed7f72235c75de6c27303f056177750108399a2
Vo Van Nghia <git@vnghia.com>
2024-11-21 14:36:18 +0100

    backend/browsing: add api/handler for get_artist_info2

---
+++ nghe-backend/src/route/browsing/get_artist_info2.rs
@@ -0,0 +1,24 @@
+use diesel::{ExpressionMethods, QueryDsl};
+use diesel_async::RunQueryDsl;
+use nghe_api::browsing::get_artist_info2::ArtistInfo2;
+pub use nghe_api::browsing::get_artist_info2::{Request, Response};
+use nghe_proc_macro::handler;
+use uuid::Uuid;
+
+use crate::database::Database;
+use crate::orm::{artists, id3};
+use crate::Error;
+
+#[handler]
+pub async fn handler(
+    database: &Database,
+    user_id: Uuid,
+    request: Request,
+) -> Result<Response, Error> {
+    let music_brainz_id = id3::artist::query::with_user_id(user_id)
+        .filter(artists::id.eq(request.id))
+        .select(artists::mbz_id)
+        .get_result(&mut database.get().await?)
+        .await?;
+    Ok(Response { artist_info2: ArtistInfo2 { music_brainz_id } })
+}
--- nghe-backend/src/route/browsing/mod.rs
+++ nghe-backend/src/route/browsing/mod.rs
@@ -1,6 +1,7 @@
 mod get_album;
 mod get_album_info2;
 mod get_artist;
+mod get_artist_info2;
 mod get_artists;
 mod get_genres;
 mod get_music_folders;
@@ -12,6 +13,7 @@ nghe_proc_macro::build_router! {
         get_album,
         get_album_info2,
         get_artist,
+        get_artist_info2,
         get_artists,
         get_genres,
         get_music_folders,
