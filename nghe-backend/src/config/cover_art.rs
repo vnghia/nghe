9faa4f3fa6eccae6c083ffff85902f0feb02bbd4
Vo Van Nghia <git@vnghia.com>
2024-11-23 21:25:46 +0100

    backend/config: add config for cover art folder file names

--- nghe-backend/src/config/cover_art.rs
+++ nghe-backend/src/config/cover_art.rs
@@ -1,6 +1,7 @@
 use derivative::Derivative;
 use serde::{Deserialize, Serialize};
-use serde_with::serde_as;
+use serde_with::formats::SpaceSeparator;
+use serde_with::{serde_as, StringWithSeparator};
 use typed_path::utils::utf8_temp_dir;
 use typed_path::Utf8NativePathBuf;
 
@@ -13,6 +14,9 @@ pub struct CoverArt {
         value = "Some(utf8_temp_dir().unwrap().join(\"nghe\").join(\"cache\").join(\"cover_art\"))"
     ))]
     pub dir: Option<Utf8NativePathBuf>,
+    #[serde_as(as = "StringWithSeparator::<SpaceSeparator, String>")]
+    #[derivative(Default(value = "vec![\"cover.jpg\".to_owned(), \"cover.png\".to_owned()]"))]
+    pub names: Vec<String>,
 }
 
 #[cfg(test)]
@@ -23,7 +27,7 @@ mod test {
 
     impl CoverArt {
         pub fn with_prefix(self, prefix: impl AsRef<Utf8NativePath>) -> Self {
-            Self { dir: self.dir.map(|_| prefix.as_ref().join("cache").join("cover_art")) }
+            Self { dir: self.dir.map(|_| prefix.as_ref().join("cache").join("cover_art")), ..self }
         }
     }
 }
