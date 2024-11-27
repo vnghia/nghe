03fccff0da46e658ff592fddf6d7e5555d89f844
Vo Van Nghia <git@vnghia.com>
2024-11-27 20:14:44 +0100

    api/playlists: add api for create playlist

---
+++ nghe-api/src/playlists/create_playlist.rs
@@ -0,0 +1,83 @@
+use nghe_proc_macro::api_derive;
+use serde_with::serde_as;
+use uuid::Uuid;
+
+// TODO: Optimize this after https://github.com/serde-rs/serde/issues/1183
+#[serde_as]
+#[api_derive(request = true, copy = false)]
+#[serde(untagged)]
+pub enum CreateOrUpdate {
+    Create {
+        name: String,
+    },
+    Update {
+        #[serde_as(as = "serde_with::DisplayFromStr")]
+        playlist_id: Uuid,
+    },
+}
+
+#[api_derive]
+#[endpoint(path = "createPlaylist")]
+pub struct Request {
+    #[serde(flatten)]
+    pub create_or_update: CreateOrUpdate,
+    #[serde(rename = "songId")]
+    pub song_ids: Option<Vec<Uuid>>,
+}
+
+#[api_derive]
+pub struct Response {
+    // TODO: Return playlist
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+    use uuid::uuid;
+
+    use super::*;
+
+    #[rstest]
+    #[case(
+        "name=ef14c42b-6efa-45f3-961c-74856fd431d5",
+        Some(Request {
+            create_or_update: CreateOrUpdate::Create {
+                name: "ef14c42b-6efa-45f3-961c-74856fd431d5".to_owned()
+            },
+            song_ids: None,
+        })
+    )]
+    #[case(
+        "name=ef14c42b-6efa-45f3-961c-74856fd431d5&\
+        songId=2b839103-04ab-4b39-9b05-8c664590eda4",
+        Some(Request {
+            create_or_update: CreateOrUpdate::Create {
+                name: "ef14c42b-6efa-45f3-961c-74856fd431d5".to_owned()
+            },
+            song_ids: Some(vec![uuid!("2b839103-04ab-4b39-9b05-8c664590eda4")]),
+        })
+    )]
+    #[case(
+        "playlistId=ef14c42b-6efa-45f3-961c-74856fd431d5",
+        Some(Request {
+            create_or_update: CreateOrUpdate::Update {
+                playlist_id: uuid!("ef14c42b-6efa-45f3-961c-74856fd431d5")
+            },
+            song_ids: None,
+        })
+    )]
+    #[case(
+        "playlistId=ef14c42b-6efa-45f3-961c-74856fd431d5&\
+        songId=2b839103-04ab-4b39-9b05-8c664590eda4",
+        Some(Request {
+            create_or_update: CreateOrUpdate::Update {
+                playlist_id: uuid!("ef14c42b-6efa-45f3-961c-74856fd431d5")
+            },
+            song_ids: Some(vec![uuid!("2b839103-04ab-4b39-9b05-8c664590eda4")]),
+        })
+    )]
+    #[case("playlistId=none", None)]
+    fn test_deserialize(#[case] url: &str, #[case] request: Option<Request>) {
+        assert_eq!(serde_html_form::from_str::<Request>(url).ok(), request);
+    }
+}
