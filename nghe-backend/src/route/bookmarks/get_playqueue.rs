1df21a1d61dc86c7ac685c81954c308990db6040
Vo Van Nghia <git@vnghia.com>
2024-11-28 22:30:04 +0100

    add test for get_playqueue

--- nghe-backend/src/route/bookmarks/get_playqueue.rs
+++ nghe-backend/src/route/bookmarks/get_playqueue.rs
@@ -47,3 +47,51 @@ pub async fn handler(
         },
     )
 }
+
+#[cfg(test)]
+mod test {
+    use fake::Fake;
+    use rstest::rstest;
+
+    use super::*;
+    use crate::route::bookmarks::save_playqueue;
+    use crate::test::{mock, Mock};
+
+    #[rstest]
+    #[tokio::test]
+    async fn test_handler(
+        #[future(awt)]
+        #[with(1, 0)]
+        mock: Mock,
+        #[values(true, false)] allow: bool,
+    ) {
+        mock.add_music_folder().allow(allow).call().await;
+        mock.add_music_folder().call().await;
+
+        let mut music_folder_permission = mock.music_folder(0).await;
+        let mut music_folder = mock.music_folder(1).await;
+
+        music_folder_permission.add_audio().n_song((2..4).fake()).call().await;
+        music_folder.add_audio().n_song((2..4).fake()).call().await;
+
+        let song_ids: Vec<_> = music_folder_permission
+            .database
+            .keys()
+            .copied()
+            .chain(music_folder.database.keys().copied())
+            .collect();
+
+        let user_id = mock.user_id(0).await;
+
+        save_playqueue::handler(
+            mock.database(),
+            user_id,
+            save_playqueue::Request { ids: song_ids, position: None, current: None },
+        )
+        .await
+        .unwrap();
+
+        let playqueue = handler(mock.database(), user_id, Request {}).await;
+        assert_eq!(playqueue.is_ok(), allow);
+    }
+}
--- nghe-backend/src/route/bookmarks/mod.rs
+++ nghe-backend/src/route/bookmarks/mod.rs
@@ -1,5 +1,5 @@
 mod get_playqueue;
-mod save_playqueue;
+pub mod save_playqueue;
 
 nghe_proc_macro::build_router! {
     modules = [get_playqueue, save_playqueue],
