075579ed00d2b0ac5ff959f0d6094ea9975f2b94
Vo Van Nghia <git@vnghia.com>
2024-11-29 06:17:49 +0100

    backend/media_annotation: add api/handler for star/unstar

--- nghe-backend/src/orm/star_artists.rs
+++ nghe-backend/src/orm/star_artists.rs
@@ -7,7 +7,37 @@ pub use crate::schema::star_artists::{self, *};
 #[derive(Debug, Insertable, AsChangeset)]
 #[diesel(table_name = star_artists, check_for_backend(crate::orm::Type))]
 #[diesel(treat_none_as_null = true)]
-pub struct Data {
+pub struct Upsert {
     pub user_id: Uuid,
     pub artist_id: Uuid,
+}
+
+mod upsert {
+    use diesel_async::RunQueryDsl;
+    use uuid::Uuid;
+
+    use super::{star_artists, Upsert};
+    use crate::database::Database;
+    use crate::Error;
+
+    impl Upsert {
+        pub async fn upserts(
+            database: &Database,
+            user_id: Uuid,
+            artist_ids: &[Uuid],
+        ) -> Result<(), Error> {
+            diesel::insert_into(star_artists::table)
+                .values(
+                    artist_ids
+                        .iter()
+                        .copied()
+                        .map(|artist_id| Self { user_id, artist_id })
+                        .collect::<Vec<_>>(),
+                )
+                .on_conflict_do_nothing()
+                .execute(&mut database.get().await?)
+                .await?;
+            Ok(())
+        }
+    }
 }
--- nghe-backend/src/orm/star_songs.rs
+++ nghe-backend/src/orm/star_songs.rs
@@ -7,7 +7,37 @@ pub use crate::schema::star_songs::{self, *};
 #[derive(Debug, Insertable, AsChangeset)]
 #[diesel(table_name = star_songs, check_for_backend(crate::orm::Type))]
 #[diesel(treat_none_as_null = true)]
-pub struct Data {
+pub struct Upsert {
     pub user_id: Uuid,
     pub song_id: Uuid,
+}
+
+mod upsert {
+    use diesel_async::RunQueryDsl;
+    use uuid::Uuid;
+
+    use super::{star_songs, Upsert};
+    use crate::database::Database;
+    use crate::Error;
+
+    impl Upsert {
+        pub async fn upserts(
+            database: &Database,
+            user_id: Uuid,
+            song_ids: &[Uuid],
+        ) -> Result<(), Error> {
+            diesel::insert_into(star_songs::table)
+                .values(
+                    song_ids
+                        .iter()
+                        .copied()
+                        .map(|song_id| Self { user_id, song_id })
+                        .collect::<Vec<_>>(),
+                )
+                .on_conflict_do_nothing()
+                .execute(&mut database.get().await?)
+                .await?;
+            Ok(())
+        }
+    }
 }
