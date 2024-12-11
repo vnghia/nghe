fcd6b0f54853aff4b11c053ffe43afc32cd676d1
Nghia <git@vnghia.com>
2024-12-11 14:53:06 +0100

    ci: run nextest for cross build (#571)
    * ci: run nextest for cross build
    
    * backend/time: sleep a little bit more
    
    * typo
    
    * typo
    
    * upload
    
    * upload even if failure

--- nghe-backend/src/time/mod.rs
+++ nghe-backend/src/time/mod.rs
@@ -3,6 +3,7 @@ use std::time::Duration;
 pub async fn now() -> time::OffsetDateTime {
     let now = time::OffsetDateTime::now_utc();
     // Sleep one microsecond because that is the highest time precision postgresql can store.
-    tokio::time::sleep(Duration::from_micros(1)).await;
+    // In test, we will sleep a little bit more just to be sure.
+    tokio::time::sleep(Duration::from_micros(if cfg!(test) { 10 } else { 1 })).await;
     now
 }
