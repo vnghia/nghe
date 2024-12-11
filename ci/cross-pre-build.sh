8b6018fc7f3e2f3281ee99afde5511a417f86fed
Vo Van Nghia <git@vnghia.com>
2024-12-11 14:02:38 +0100

    ci: run nextest for cross build

--- ci/cross-pre-build.sh
+++ ci/cross-pre-build.sh
@@ -4,3 +4,5 @@ set -euxo pipefail
 if [[ "$CROSS_TARGET" == "aarch64-unknown-linux-gnu" ]]; then
   apt-get update && apt-get --assume-yes install libclang-10-dev
 fi
+
+curl -LsSf https://get.nexte.st/latest/linux | tar zxf - -C ${CARGO_HOME:-~/.cargo}/bin
