0f94e40d0e9c5a85bbd91ff65e29a5b024fbca8c
Vo Van Nghia <git@vnghia.com>
2024-12-23 21:47:15 +0100

    add test for lyrics

--- nghe-backend/src/file/lyrics/mod.rs
+++ nghe-backend/src/file/lyrics/mod.rs
@@ -72,7 +72,11 @@ impl<'a> TryFrom<&'a BinaryFrame<'_>> for Lyrics<'a> {
 
 impl<'a> Lyrics<'a> {
     pub fn from_unsync_text(content: &'a str) -> Self {
-        Self { description: None, language: Language::Und, lines: content.lines().collect() }
+        Self {
+            description: None,
+            language: Language::Und,
+            lines: content.lines().filter(|text| !text.is_empty()).collect(),
+        }
     }
 
     pub fn from_sync_text(content: &str) -> Result<Self, Error> {
@@ -88,17 +92,30 @@ impl<'a> Lyrics<'a> {
                 .lines
                 .into_iter()
                 .map(|line| {
-                    let duration: time::Duration = line.time.to_duration().try_into()?;
-                    Ok::<_, Error>((duration, line.text))
+                    let minutes: i64 = line.time.minutes.into();
+                    let seconds: i64 = line.time.seconds.into();
+                    let milliseconds: i64 = line.time.millis.unwrap_or_default().into();
+                    let duration = time::Duration::milliseconds(
+                        minutes * 60 * 1000 + seconds * 1000 + milliseconds * 10,
+                    );
+                    (duration, line.text)
                 })
-                .try_collect()?,
+                .collect(),
         })
+    }
+}
+
+impl Lyrics<'_> {
+    pub fn is_sync(&self) -> bool {
+        matches!(self.lines, Lines::Sync(_))
     }
 }
 
 #[cfg(test)]
 #[coverage(off)]
 mod test {
+    use std::fmt::Display;
+
     use fake::{Dummy, Fake, Faker};
     use itertools::Itertools;
 
@@ -116,7 +133,19 @@ mod test {
                 Self {
                     description: config.fake_with_rng::<Option<String>, _>(rng).map(Cow::Owned),
                     language: Language::from_usize((0..=7915).fake()).unwrap(),
-                    lines: fake::vec![(u32, String); 1..=5].into_iter().sorted().collect(),
+                    lines: fake::vec![String; 1..=5]
+                        .into_iter()
+                        .map(|text| {
+                            (
+                                (((0..100).fake::<u32>() * 60 * 100
+                                    + (0..60).fake::<u32>() * 100
+                                    + (0..99).fake::<u32>())
+                                    * 10),
+                                text,
+                            )
+                        })
+                        .sorted()
+                        .collect(),
                 }
             } else {
                 Self {
@@ -126,5 +155,83 @@ mod test {
                 }
             }
         }
+    }
+
+    impl Display for Lyrics<'_> {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            match &self.lines {
+                Lines::Unsync(lines) => write!(f, "{}", lines.join("\n")),
+                Lines::Sync(lines) => {
+                    if let Some(description) = self.description.as_ref() {
+                        writeln!(f, "[desc:{description}]")?;
+                    }
+                    writeln!(f, "[lang:{}]\n", self.language)?;
+
+                    for (duration, text) in lines {
+                        let seconds = duration.0.whole_seconds();
+                        let minutes = seconds / 60;
+                        let seconds = seconds % 60;
+                        let milliseconds = duration.0.subsec_milliseconds() / 10;
+                        write!(f, "[{minutes:02}:{seconds:02}.{milliseconds:02}]")?;
+                        writeln!(f, "{text}")?;
+                    }
+
+                    Ok(())
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+#[coverage(off)]
+mod tests {
+    use fake::{Fake, Faker};
+    use rstest::rstest;
+
+    use super::*;
+    use crate::test::assets;
+
+    #[rstest]
+    fn test_lyrics_roundtrip() {
+        let lyrics: Lyrics<'_> = Faker.fake();
+        if lyrics.is_sync() {
+            assert_eq!(lyrics, Lyrics::from_sync_text(&lyrics.to_string()).unwrap());
+        } else {
+            assert_eq!(lyrics, Lyrics::from_unsync_text(&lyrics.to_string()));
+        }
+    }
+
+    #[rstest]
+    #[case("sync.lrc", Lyrics {
+        description: Some("Lyrics".to_owned().into()),
+        language: Language::Eng,
+        lines: vec![
+            (1020_u32, "Hello hi".to_owned()),
+            (3040_u32, "Bonjour salut".to_owned()),
+            (5060_u32, "おはよう こんにちは".to_owned()),
+        ]
+        .into_iter()
+        .collect()
+    })]
+    #[case("unsync.txt", Lyrics {
+        description: None,
+        language: Language::Und,
+        lines: vec![
+            "Hello hi",
+            "Bonjour salut",
+            "おはよう こんにちは",
+        ]
+        .into_iter()
+        .collect()
+    })]
+    fn test_from_text(#[case] filename: &str, #[case] lyrics: Lyrics<'_>) {
+        let content = std::fs::read_to_string(assets::dir().join("lyrics").join(filename)).unwrap();
+        let parsed = if lyrics.is_sync() {
+            Lyrics::from_sync_text(&content).unwrap()
+        } else {
+            Lyrics::from_unsync_text(&content)
+        };
+        assert_eq!(parsed, lyrics);
     }
 }
