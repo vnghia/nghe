1684a1e19cabd66167d46905ccc2e95e2063ef8b
Vo Van Nghia <git@vnghia.com>
2024-12-24 08:41:53 +0100

    backend: add api/handler for get_lyrics_by_song_id

--- nghe-backend/src/route/media_retrieval/mod.rs
+++ nghe-backend/src/route/media_retrieval/mod.rs
@@ -1,11 +1,12 @@
 pub mod download;
 mod get_cover_art;
+mod get_lyrics_by_song_id;
 mod stream;
 
 use crate::config;
 
 nghe_proc_macro::build_router! {
-    modules = [download, get_cover_art, stream],
+    modules = [download, get_cover_art, get_lyrics_by_song_id, stream],
     filesystem = true,
     extensions = [config::Transcode, config::CoverArt],
 }
