fb6e428412e41d4eb6828056b2193e2dee3ec512
Vo Van Nghia <git@vnghia.com>
2025-01-05 19:28:10 +0100

    add a reusable form

--- nghe-frontend/src/components/setup.rs
+++ nghe-frontend/src/components/setup.rs
@@ -20,52 +20,34 @@ pub fn Setup() -> impl IntoView {
                          dark:text-white",
                     )
                     .child("Nghe"),
-                html::div()
-                    .class(
-                        "w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 \
-                         dark:bg-gray-800 dark:border-gray-700",
-                    )
-                    .child(
-                        html::div().class("p-6 space-y-4 md:space-y-6 sm:p-8").child((
-                            html::h1()
-                                .class(
-                                    "text-xl font-bold leading-tight tracking-tight text-gray-900 \
-                                     md:text-2xl dark:text-white",
-                                )
-                                .child("Setup admin account"),
-                            html::div().class("space-y-4 md:space-y-6").child((
-                                form::input::Text(
-                                    "username", "Username", "username", "username", "username",
-                                    username,
-                                ),
-                                form::input::Text(
-                                    "email",
-                                    "Email",
-                                    "email",
-                                    "email",
-                                    "email@example.com",
-                                    email,
-                                ),
-                                form::input::Text(
-                                    "password",
-                                    "Password",
-                                    "password",
-                                    "password",
-                                    "••••••••",
-                                    password,
-                                ),
-                                html::button()
-                                    .class(
-                                        "w-full text-white bg-primary-600 hover:bg-primary-700 \
-                                         focus:ring-4 focus:outline-none focus:ring-primary-300 \
-                                         font-medium rounded-lg text-sm px-5 py-2.5 text-center \
-                                         dark:bg-primary-600 dark:hover:bg-primary-700 \
-                                         dark:focus:ring-primary-800",
-                                    )
-                                    .child("Setup"),
-                            )),
-                        )),
-                    ),
+                form::Form(
+                    "Setup admin account",
+                    move || {
+                        (
+                            form::input::Text(
+                                "username", "Username", "username", "username", "username",
+                                username,
+                            ),
+                            form::input::Text(
+                                "email",
+                                "Email",
+                                "email",
+                                "email",
+                                "email@example.com",
+                                email,
+                            ),
+                            form::input::Text(
+                                "password",
+                                "Password",
+                                "password",
+                                "password",
+                                "••••••••",
+                                password,
+                            ),
+                        )
+                    },
+                    "Setup",
+                ),
             )),
     )
 }
