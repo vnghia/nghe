41ab7e1354509abd02826484e6e66c0f5fa9c0d8
Vo Van Nghia <git@vnghia.com>
2025-01-09 02:10:04 +0100

    add login to authentication trait

--- nghe-backend/src/http/extract/auth/mod.rs
+++ nghe-backend/src/http/extract/auth/mod.rs
@@ -1,18 +1,29 @@
 pub mod form;
 pub mod header;
 mod username;
-
 use axum::extract::FromRef;
-use diesel::{ExpressionMethods, QueryDsl};
-use diesel_async::RunQueryDsl;
 pub use form::Form;
 pub use header::Header;
-use uuid::Uuid;
 
 use crate::database::Database;
 use crate::orm::users;
 use crate::{Error, error};
 
-pub trait Authentication {
-    async fn authenticate(&self, database: &Database) -> Result<users::Authenticated, Error>;
+pub trait Authentication: Sized {
+    async fn authenticated(&self, database: &Database) -> Result<users::Authenticated, Error>;
+
+    async fn login<S, R>(&self, state: &S) -> Result<users::Authenticated, Error>
+    where
+        S: Send + Sync,
+        Database: FromRef<S>,
+        R: Authorization,
+    {
+        let database = Database::from_ref(state);
+        let user = self.authenticated(&database).await?;
+        if R::authorized(user.role) { Ok(user) } else { error::Kind::Forbidden.into() }
+    }
+}
+
+pub trait Authorization {
+    fn authorized(role: users::Role) -> bool;
 }
