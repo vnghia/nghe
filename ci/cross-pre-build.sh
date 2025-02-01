20764bb622a3829e176e49aa54407cc811f017b2
Nghia <git@vnghia.com>
2025-02-01 02:36:08 +0100

    ci: fix freebsd ci (#709)
    * ci: fix freebsd ci
    
    * bump vcpkg version
    
    * add nightly to cargo install
    
    * install nightly toolchain
    
    * add cargo env
    
    * use toolchain
    
    * use package freebsd
    
    * pin to specific rust version
    
    * pin to rust version
    
    * enable other pipeline
    
    * dont pass rust backtrace to freebsd https://github.com/rust-lang/rust/issues/132185
    
    * add a newline

--- ci/cross-pre-build.sh
+++ ci/cross-pre-build.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-if [[ "$CROSS_TARGET" == "aarch64-unknown-linux-gnu" ]]; then
-  apt-get update && apt-get --assume-yes install libclang-10-dev
-fi
-
 curl -LsSf https://get.nexte.st/latest/linux | tar zxf - -C /usr/bin/
