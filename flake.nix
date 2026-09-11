53f0930af1b1eef2388e7c2230a76642b3b08524
Vo Van Nghia <git@vnghia.com>
2026-09-11 20:59:01 +0200

    build ffmpeg from devshell

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-unstable";
+      url = "github:nixos/nixpkgs/nixos-26.05";
     };
 
     flake-parts = {
@@ -13,10 +13,6 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  };
-
-  nixConfig = {
-    allow-import-from-derivation = true;
   };
 
   outputs =
@@ -41,35 +37,136 @@
           ...
         }:
         let
-          rustToolchain = inputs.fenix.packages.${system}.fromToolchainFile {
+          toolchain = inputs.fenix.packages.${system}.fromToolchainFile {
             file = ./rust-toolchain.toml;
             sha256 = "sha256-qaiVSNjXbvb8Jd+Zo0y59MrGs8KQwCQ/QwSm4Gn4Lxs=";
           };
+
+          static = rec {
+            openssl = pkgs.openssl.override { static = true; };
+
+            lame =
+              (pkgs.lame.override {
+                frontendSupport = false;
+              }).overrideAttrs
+                (
+                  finalAttrs: previousAttrs: {
+                    configureFlags = previousAttrs.configureFlags ++ [
+                      "--disable-shared"
+                      "--enable-static"
+                    ];
+                  }
+                );
+
+            libopus = pkgs.libopus.overrideAttrs (
+              finalAttrs: previousAttrs: {
+                mesonBuildType = "release";
+                mesonFlags = previousAttrs.mesonFlags ++ [
+                  "-Ddefault_library=static"
+                  "-Ddefault_both_libraries=static"
+                ];
+              }
+            );
+
+            soxr = pkgs.soxr.overrideAttrs (
+              finalAttrs: previousAttrs: {
+                cmakeFlags = previousAttrs.cmakeFlags ++ [
+                  "-DBUILD_SHARED_LIBS=OFF"
+                  "-DWITH_OPENMP=OFF"
+                ];
+              }
+            );
+
+            ffmpeg =
+              (pkgs.ffmpeg.override {
+                version = "8.0.3";
+
+                ffmpegVariant = "headless";
+
+                withHeadlessDeps = false;
+                withSmallDeps = false;
+                withFullDeps = false;
+
+                withMp3lame = true;
+                withOpus = true;
+                withSoxr = true;
+                withVorbis = true;
+
+                withSmallBuild = false;
+                withHardcodedTables = true;
+                withMultithread = true;
+                withNetwork = true;
+                withPixelutils = true;
+                withStatic = true;
+                withShared = false;
+                withPic = true;
+                withThumb = false;
+
+                buildFfmpeg = false;
+                buildFfplay = false;
+                buildFfprobe = false;
+                buildQtFaststart = false;
+                buildAvcodec = true;
+                buildAvdevice = true;
+                buildAvfilter = true;
+                buildAvformat = true;
+                buildAvutil = true;
+                buildSwresample = true;
+                buildSwscale = true;
+
+                withOptimisations = true;
+
+                inherit lame;
+                inherit libopus;
+                inherit soxr;
+              }).overrideAttrs
+                (
+                  finalAttrs: previousAttrs: {
+                    postPatch = previousAttrs.postPatch + ''
+                      substituteInPlace configure \
+                        --replace-fail \
+                          'require libsoxr soxr.h soxr_create -lsoxr' \
+                          'require libsoxr soxr.h soxr_create -lsoxr $libm_extralibs'
+                    '';
+                    configureFlags = previousAttrs.configureFlags ++ [
+                      "--enable-openssl"
+                    ];
+                    buildInputs = previousAttrs.buildInputs ++ [
+                      openssl
+                    ];
+                  }
+                );
+          };
         in
         {
+          packages = static;
+
           devShells.default =
             with pkgs;
             mkShell {
               env = {
+                OPENSSL_INCLUDE_DIR = "${static.openssl.dev}/include";
+                OPENSSL_LIB_DIR = "${static.openssl.out}/lib";
+
                 LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+
                 LD_LIBRARY_PATH = lib.makeLibraryPath [
                   postgresql.lib
                 ];
               };
 
               packages = [
-                rustToolchain
-
-                # ffmpeg
-                zip
-                unzip
+                toolchain
                 pkg-config
-                python3
-                nasm
+
+                # native
+                stdenv.cc
+
+                static.openssl
 
                 clang
                 llvmPackages.libclang
-                stdenv.cc
+                static.ffmpeg
 
                 # test
                 postgresql.lib
