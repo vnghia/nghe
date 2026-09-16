a203e8a23f5616609b0188826c83d84002f0e8e3
Vo Van Nghia <git@vnghia.com>
2026-09-16 23:40:50 +0200

    support cross compiling

--- flake.nix
+++ flake.nix
@@ -15,6 +15,11 @@
     };
   };
 
+  nixConfig = {
+    extra-substituters = "https://nghe.cachix.org";
+    extra-trusted-public-keys = "nghe.cachix.org-1:H757ngFI7o6unzi/uiXKN6fhfQjCjrEI80hZDWr0Mrs=";
+  };
+
   outputs =
     inputs@{
       self,
@@ -42,133 +47,197 @@
             sha256 = "sha256-qaiVSNjXbvb8Jd+Zo0y59MrGs8KQwCQ/QwSm4Gn4Lxs=";
           };
 
-          static = rec {
-            openssl = pkgs.openssl.override { static = true; };
-
-            lame =
-              (pkgs.lame.override {
-                frontendSupport = false;
-              }).overrideAttrs
-                (
-                  finalAttrs: previousAttrs: {
-                    configureFlags = previousAttrs.configureFlags ++ [
-                      "--disable-shared"
-                      "--enable-static"
-                    ];
-                  }
-                );
-
-            libopus = pkgs.libopus.overrideAttrs (
-              finalAttrs: previousAttrs: {
-                mesonBuildType = "release";
-                mesonFlags = previousAttrs.mesonFlags ++ [
-                  "-Ddefault_library=static"
-                  "-Ddefault_both_libraries=static"
-                ];
-              }
-            );
-
-            soxr = pkgs.soxr.overrideAttrs (
-              finalAttrs: previousAttrs: {
-                cmakeFlags = previousAttrs.cmakeFlags ++ [
-                  "-DBUILD_SHARED_LIBS=OFF"
-                  "-DWITH_OPENMP=OFF"
-                ];
-              }
-            );
-
-            libogg = pkgs.libogg.overrideAttrs (
-              finalAttrs: previousAttrs: {
-                cmakeFlags = previousAttrs.cmakeFlags ++ [
-                  "-DBUILD_SHARED_LIBS=OFF"
-                ];
-              }
-            );
-
-            libvorbis = (pkgs.libvorbis.override { inherit libogg; }).overrideAttrs (
-              finalAttrs: previousAttrs: {
-                configureFlags = [
-                  "--disable-shared"
-                  "--enable-static"
-                ];
-              }
-            );
-
-            ffmpeg =
-              (pkgs.ffmpeg.override {
-                version = "8.0.3";
-
-                ffmpegVariant = "headless";
-
-                withHeadlessDeps = false;
-                withSmallDeps = false;
-                withFullDeps = false;
-
-                withMp3lame = true;
-                withOpus = true;
-                withSoxr = true;
-                withVorbis = true;
-
-                withSmallBuild = false;
-                withHardcodedTables = true;
-                withMultithread = true;
-                withNetwork = true;
-                withPixelutils = true;
-                withStatic = true;
-                withShared = false;
-                withPic = true;
-                withThumb = false;
-
-                buildFfmpeg = false;
-                buildFfplay = false;
-                buildFfprobe = false;
-                buildQtFaststart = false;
-                buildAvcodec = true;
-                buildAvdevice = true;
-                buildAvfilter = true;
-                buildAvformat = true;
-                buildAvutil = true;
-                buildSwresample = true;
-                buildSwscale = true;
-
-                withOptimisations = true;
-                withStripping = true;
-
-                inherit lame;
-                inherit libopus;
-                inherit soxr;
-                inherit libvorbis;
-              }).overrideAttrs
-                (
-                  finalAttrs: previousAttrs: {
-                    configureFlags = previousAttrs.configureFlags ++ [
-                      "--enable-openssl"
-                      "--pkg-config-flags=--static"
-                      "--extra-libs=-logg"
-                      "--extra-libs=-lm"
-                    ];
-                    buildInputs = previousAttrs.buildInputs ++ [
-                      openssl
-                    ];
-                  }
-                );
+          rustTargetMap = {
+            "x86_64-linux" = "x86_64-unknown-linux-gnu";
+            "aarch64-linux" = "aarch64-unknown-linux-gnu";
+            "musl64" = "x86_64-unknown-linux-musl";
           };
-        in
-        rec {
-          packages = static;
 
-          devShells.default =
-            with pkgs;
-            mkShell {
+          mkNativeDeps =
+            {
+              hostPkgs,
+              static ? true,
+            }:
+            let
+              hostLib = hostPkgs.lib;
+
+              disableTarget = if static then "shared" else "static";
+              enableTarget = if static then "static" else "shared";
+              sharedLibs = if static then "OFF" else "ON";
+            in
+            rec {
+              openssl = hostPkgs.openssl.override { inherit static; };
+
+              lame =
+                (hostPkgs.lame.override {
+                  frontendSupport = false;
+                }).overrideAttrs
+                  (
+                    finalAttrs: previousAttrs: {
+                      configureFlags = previousAttrs.configureFlags ++ [
+                        "--disable-${disableTarget}"
+                        "--enable-${enableTarget}"
+                      ];
+                    }
+                  );
+
+              libopus = hostPkgs.libopus.overrideAttrs (
+                finalAttrs: previousAttrs: {
+                  mesonBuildType = "release";
+                  mesonFlags = previousAttrs.mesonFlags ++ [
+                    "-Ddefault_library=${enableTarget}"
+                    "-Ddefault_both_libraries=${enableTarget}"
+                  ];
+                }
+              );
+
+              soxr = hostPkgs.soxr.overrideAttrs (
+                finalAttrs: previousAttrs: {
+                  cmakeFlags = previousAttrs.cmakeFlags ++ [
+                    "-DWITH_OPENMP=OFF"
+                    "-DBUILD_SHARED_LIBS=${sharedLibs}"
+                  ];
+                }
+              );
+
+              libogg = hostPkgs.libogg.overrideAttrs (
+                finalAttrs: previousAttrs: {
+                  cmakeFlags = previousAttrs.cmakeFlags ++ [
+                    "-DBUILD_SHARED_LIBS=${sharedLibs}"
+                  ];
+                }
+              );
+
+              libvorbis = (hostPkgs.libvorbis.override { inherit libogg; }).overrideAttrs (
+                finalAttrs: previousAttrs: {
+                  configureFlags = [
+                    "--disable-${disableTarget}"
+                    "--enable-${enableTarget}"
+                  ];
+                }
+              );
+
+              ffmpeg =
+                (hostPkgs.ffmpeg.override {
+                  version = "8.0.3";
+
+                  ffmpegVariant = "headless";
+
+                  withHeadlessDeps = false;
+                  withSmallDeps = false;
+                  withFullDeps = false;
+
+                  withMp3lame = true;
+                  withOpus = true;
+                  withSoxr = true;
+                  withVorbis = true;
+
+                  withSmallBuild = false;
+                  withHardcodedTables = true;
+                  withMultithread = true;
+                  withNetwork = true;
+                  withPixelutils = true;
+                  withStatic = static;
+                  withShared = !static;
+                  withPic = true;
+                  withThumb = false;
+
+                  buildFfmpeg = false;
+                  buildFfplay = false;
+                  buildFfprobe = false;
+                  buildQtFaststart = false;
+                  buildAvcodec = true;
+                  buildAvdevice = true;
+                  buildAvfilter = true;
+                  buildAvformat = true;
+                  buildAvutil = true;
+                  buildSwresample = true;
+                  buildSwscale = true;
+
+                  withOptimisations = true;
+                  withStripping = true;
+
+                  inherit lame;
+                  inherit libopus;
+                  inherit soxr;
+                  inherit libvorbis;
+                }).overrideAttrs
+                  (
+                    finalAttrs: previousAttrs: {
+                      configureFlags =
+                        previousAttrs.configureFlags
+                        ++ [
+                          "--enable-openssl"
+                          "--extra-libs=-lm"
+                        ]
+                        ++ (hostLib.optional static "--pkg-config-flags=--static");
+                      buildInputs = previousAttrs.buildInputs ++ [
+                        openssl
+                      ];
+                    }
+                  );
+
+              # test
+              libpq =
+                (hostPkgs.libpq.override {
+                  curlSupport = false;
+                  gssSupport = false;
+                  nlsSupport = false;
+                }).overrideAttrs
+                  (
+                    finalAttrs: previousAttrs: {
+                      dontDisableStatic = static;
+                    }
+                  );
+            };
+
+          mkDevShell =
+            {
+              target ? system,
+              static ? true,
+            }:
+            let
+              isCross = target != system;
+              hostPkgs =
+                if isCross then
+                  import nixpkgs {
+                    localSystem = {
+                      inherit system;
+                    };
+                    crossSystem = {
+                      system = pkgs.pkgsCross.${target}.stdenv.hostPlatform.config;
+                    };
+                  }
+                else
+                  pkgs;
+              hostLib = hostPkgs.lib;
+
+              rustTarget = rustTargetMap.${target};
+              rustShoutTarget = builtins.replaceStrings [ "-" ] [ "_" ] (hostLib.toUpper rustTarget);
+              rustPlatform = hostPkgs.makeRustPlatform {
+                cargo = toolchain;
+                rustc = toolchain;
+              };
+
+              nativeDeps = mkNativeDeps {
+                inherit hostPkgs;
+                inherit static;
+              };
+
+              ccBin = "${hostPkgs.stdenv.cc}/bin/${hostLib.optionalString isCross "${rustTarget}-"}cc";
+            in
+            with hostPkgs;
+            pkgs.mkShellNoCC {
               env = {
-                OPENSSL_INCLUDE_DIR = "${static.openssl.dev}/include";
-                OPENSSL_LIB_DIR = "${static.openssl.out}/lib";
+                # cargo
+                CARGO_BUILD_TARGET = rustTarget;
+                "CARGO_TARGET_${rustShoutTarget}_LINKER" = ccBin;
+                "CC_${rustShoutTarget}" = ccBin;
 
-                LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
-
-                LD_LIBRARY_PATH = lib.makeLibraryPath [
-                  postgresql.lib
-                ];
+                # native
+                OPENSSL_INCLUDE_DIR = "${nativeDeps.openssl.dev}/include";
+                OPENSSL_LIB_DIR = "${nativeDeps.openssl.out}/lib";
+                OPENSSL_STATIC = if static then "1" else "0";
               };
 
               packages = [
@@ -176,14 +245,19 @@
                 pkg-config
 
                 # native
-                clang
-                llvmPackages.libclang
-
-                # test
-                postgresql.lib
+                stdenv.cc
+                llvmPackages.libclang.lib
+                rustPlatform.bindgenHook
               ]
-              ++ (pkgs.lib.attrValues packages);
+              ++ (hostLib.attrValues nativeDeps)
+              ++ hostLib.optional stdenv.hostPlatform.isLinux pkgs.autoPatchelfHook;
             };
+        in
+        {
+          devShells = {
+            default = mkDevShell { };
+          }
+          // (pkgs.lib.mapAttrs (target: _: mkDevShell { inherit target; }) rustTargetMap);
         };
     };
 }
