d395d8057bb1dce4dbb912e5140ec3f37a870f3e
Vo Van Nghia <git@vnghia.com>
2026-09-17 21:32:11 +0200

    bump rust nightly

--- flake.nix
+++ flake.nix
@@ -44,7 +44,7 @@
         let
           toolchain = inputs.fenix.packages.${system}.fromToolchainFile {
             file = ./rust-toolchain.toml;
-            sha256 = "sha256-qaiVSNjXbvb8Jd+Zo0y59MrGs8KQwCQ/QwSm4Gn4Lxs=";
+            sha256 = "sha256-fya6m+K07kzjq4k6cRusPfL67fsS6kwemDTRtc6sFQQ=";
           };
 
           rustTargetMap = {
