102482ce668f39e28005fed22a4c329040e7597f
Vo Van Nghia <git@vnghia.com>
2026-09-19 19:22:40 +0200

    use podman

--- flake.nix
+++ flake.nix
@@ -256,7 +256,7 @@
             pkgs.mkShellNoCC {
               dontAddExtraLibs = true;
 
-              env = {
+              env = rec {
                 # cargo
                 CARGO_BUILD_TARGET = rustTarget;
                 "CARGO_TARGET_${rustShoutTarget}_LINKER" = ccBin;
@@ -270,6 +270,20 @@
                 OPENSSL_STATIC = if withStatic then "1" else "0";
 
                 PQ_LIB_STATIC = if withStatic then "1" else null;
+
+                # test
+                POSTGRES_USER = "postgres";
+                POSTGRES_PASSWORD = "postgres";
+                POSTGRES_DATABASE = "postgres";
+                POSTGRES_PORT = 5432;
+                DATABASE_URL = "postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:${POSTGRES_PORT}/${POSTGRES_DATABASE}";
+
+                AWS_ACCESS_KEY_ID = "key";
+                AWS_SECRET_ACCESS_KEY = "key";
+                AWS_REGION = "us-east-1";
+                AWS_PORT = 9090;
+                AWS_USE_PATH_STYLE_ENDPOINT = "true";
+                AWS_ENDPOINT_URL = "http://localhost:${AWS_PORT}";
               };
 
               packages = [
