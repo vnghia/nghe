f64f9e8c9926ea13108eba8701d371868891ce91
Vo Van Nghia <git@vnghia.com>
2026-09-23 22:33:28 +0200

    use new array api for serde

--- nghe-backend/src/config/database.rs
+++ nghe-backend/src/config/database.rs
@@ -1,5 +1,5 @@
 use educe::Educe;
-use serde::{Deserialize, Deserializer, de};
+use serde::Deserialize;
 use serde_with::serde_as;
 
 use crate::database::Key;
@@ -10,15 +10,7 @@ use crate::database::Key;
 pub struct Database {
     #[educe(Debug(ignore))]
     pub url: String,
-    #[serde(deserialize_with = "deserialize")]
+    #[serde(with = "faster_hex::nopfx_ignorecase::array")]
     #[educe(Debug(ignore))]
     pub key: Key,
-}
-
-pub fn deserialize<'de, D>(deserializer: D) -> Result<Key, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let data: Vec<u8> = faster_hex::nopfx_ignorecase::deserialize(deserializer)?;
-    data.try_into().map_err(|_| de::Error::custom("Could not convert vector to array of length 16"))
 }
